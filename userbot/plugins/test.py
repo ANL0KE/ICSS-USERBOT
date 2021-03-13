a899aeecac9f2500942b1eec08c713033298d355
ANL0KE <79422815+ANL0KE@users.noreply.github.com>
2021-03-13 11:35:33 +0000

    pylint: auto fixes

--- userbot/plugins/test.py
+++ userbot/plugins/test.py
@@ -1,7 +1,8 @@
 import asyncio
 
+
 @icssbot.on(admin_cmd(pattern="هيج", outgoing=True))
 async def icss(ics):
-   await ics.edit(" هها هلو")
-   await asyncio.sleep(1)
-   await ics.edit(" هها هلو")
+    await ics.edit(" هها هلو")
+    await asyncio.sleep(1)
+    await ics.edit(" هها هلو")
