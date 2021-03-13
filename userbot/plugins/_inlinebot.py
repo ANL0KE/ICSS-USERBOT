652661d121e61db3b2d3c2e53c11299ca4fa9f9d
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-13 12:01:32 +0300

    ICSS

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -247,7 +247,7 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
             )
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
-    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"اغلاق")))
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
     async def on_plug_in_callback_query_handler(event):
         if event.query.user_id == bot.uid:
             await event.edit("⫷ القائمه مغلقه ⫸")
