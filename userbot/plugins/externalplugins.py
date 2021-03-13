c0ebf20b318870c681f21efbb1bf3f89fec3cc7f
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-13 18:16:05 +0300

    ICSS

---
+++ userbot/plugins/externalplugins.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+
+from telethon.tl.types import InputMessagesFilterDocument
+
+from ..utils import load_module
+from . import BOTLOG_CHATID
+
+if Config.PLUGIN_CHANNEL:
+
+    async def install():
+        documentss = await bot.get_messages(
+            Config.PLUGIN_CHANNEL, None, filter=InputMessagesFilterDocument
+        )
+        total = int(documentss.total)
+        for module in range(total):
+            plugin_to_install = documentss[module].id
+            plugin_name = documentss[module].file.name
+            if os.path.exists(f"userbot/plugins/{plugin_name}"):
+                return
+            downloaded_file_name = await bot.download_media(
+                await bot.get_messages(Config.PLUGIN_CHANNEL, ids=plugin_to_install),
+                "userbot/plugins/",
+            )
+            path1 = Path(downloaded_file_name)
+            shortname = path1.stem
+            load_module(shortname.replace(".py", ""))
+            await bot.send_message(
+                BOTLOG_CHATID,
+                f"Installed Plugin `{os.path.basename(downloaded_file_name)}` successfully.",
+            )
+
+    bot.loop.create_task(install())
