02f29d93c24c2e677011d99c164adf389ff408db
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-14 21:39:47 +0300

    ICSS

---
+++ userbot/plugins/scramble.py
@@ -0,0 +1,28 @@
+import random
+import re
+
+from userbot.utils import admin_cmd
+
+# ===================================================== #
+
+@icssbot.on(admin_cmd(pattern=r"scramble(\s+[\S\s]+|$)"))
+async def scramble_message(i):
+    reply_message = await i.get_reply_message()
+    text = i.pattern_match.group(1) or reply_message.text
+    words = re.split(r"\s", text)
+    scrambled = map(scramble_word, words)
+    text = " ".join(scrambled)
+    await i.edit(text)
+
+# ===================================================== #
+
+def scramble_word(word):
+    if len(word) < 4:                      
+        return word                        
+
+    first_letter = word[0]                 
+    last_letter = word[-1]                 
+    middle_letters = list(word[1:-1])
+    random.shuffle(middle_letters)
+
+    return first_letter + "".join(middle_letters) + last_letter
