8db5a81947469e83bd02ddd5755852a2a9635f66
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-14 13:12:52 +0300

    ICSS

--- userbot/plugins/kut_gif3.py
+++ userbot/plugins/kut_gif3.py
@@ -1,32 +1,29 @@
-import time
+# KutGif for icss by: @rruuurr
 
-from . import StartTime, get_readable_time, reply_id
+from . import reply_id
 
-DEFAULTUSER = "ICSS"
 ICSS_IMG = "https://telegra.ph/file/78185fe07950955f07e74.mp4"
-CUSTOM_ICSS_TEXT = "𓆩 𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺 - 𝑪𝑼𝑻𝑬 𝑮𝑰𝑭 𓆪"
-EMOJI = "  ↫ "
+ICSS_TEXT = "𓆩 𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺 - 𝑪𝑼𝑻𝑬 𝑮𝑰𝑭 𓆪"
 
 
 @icssbot.on(admin_cmd(outgoing=True, pattern="ك3$"))
 @icssbot.on(sudo_cmd(pattern="ك3$", allow_sudo=True))
 async def kutgif(icss):
-    if alive.fwd_from:
+    if icss.fwd_from:
         return
     reply_to_id = await reply_id(icss)
-    await get_readable_time((time.time() - StartTime))
     if ICSS_IMG:
-        icss_caption = f"**{CUSTOM_ICSS_TEXT}**\n"
+        icss_caption = f"**{ICSS_TEXT}**\n"
         icss_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐢𝐜𝐬𝐬ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
         icss_caption += f"**↫ المتـحركه الثالثه 𓆰.**"
         await icss.client.send_file(
-            icss.chat_id, CAT_IMG=ICSS_IMG, caption=icss_caption, reply_to=reply_to_id
+            icss.chat_id, ICSS_IMG, caption=icss_caption, reply_to=reply_to_id
         )
         await icss.delete()
     else:
         await edit_or_reply(
             icss,
-            f"**{CUSTOM_ICSS_TEXT}**\n"
+            f"**{ICSS_TEXT}**\n"
             f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐢𝐜𝐬𝐬ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
             f"**↫ المتـحركه الثالثه 𓆰.**",
         )
