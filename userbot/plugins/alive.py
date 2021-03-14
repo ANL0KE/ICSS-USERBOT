bfabe7d19592538f5158e4fa78658a15721c7a3b
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-14 13:30:53 +0300

    ICSS

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,14 +1,16 @@
+# alive - ©icss
+
 import time
 from platform import python_version
 
 from telethon import version
 
-from . import ALIVE_NAME, StartTime, catversion, get_readable_time, mention, reply_id
+from . import ALIVE_NAME, StartTime, icssversion, get_readable_time, mention, reply_id
 
 DEFAULTUSER = ALIVE_NAME or "ICSS"
 ICSS_IMG = Config.ALIVE_PIC or "https://telegra.ph/file/499596b18292c0e43ac56.jpg"
-CUSTOM_ALIVE_TEXT = Config.CUSTOM_ALIVE_TEXT or "𓆩 𝑾𝑬𝑳𝑪𝑶𝑴𝑬 𝑻𝑶 𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺 𓆪"
-EMOJI = Config.CUSTOM_ALIVE_EMOJI or "  - ❝ ⌊ "
+ICSS_TEXT = Config.CUSTOM_ALIVE_TEXT or "𓆩 𝑾𝑬𝑳𝑪𝑶𝑴𝑬 𝑻𝑶 𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺 𓆪"
+E_ICSS = Config.CUSTOM_ALIVE_EMOJI or "  - ❝ ⌊ "
 
 
 @icssbot.on(admin_cmd(outgoing=True, pattern="السورس$"))
@@ -20,15 +22,15 @@ async def icssalive(icss):
     uptime = await get_readable_time((time.time() - StartTime))
     _, check_sgnirts = check_data_base_heal_th()
     if ICSS_IMG:
-        icss_caption = f"**{CUSTOM_ALIVE_TEXT}**\n"
+        icss_caption = f"**{ICSS_TEXT}**\n"
         icss_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐢𝐜𝐬𝐬ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
-        icss_caption += f"**{EMOJI} قاعدة البيانات ↫** `{check_sgnirts}`\n"
-        icss_caption += f"**{EMOJI} اصدار التليثون  ↫** `{version.__version__}\n`"
-        icss_caption += f"**{EMOJI} اصدار اڪسس ↫** `{catversion}`\n"
-        icss_caption += f"**{EMOJI} اصدار البايثون ↫** `{python_version()}\n`"
+        icss_caption += f"**{E_ICSS} قاعدة البيانات ↫** `{check_sgnirts}`\n"
+        icss_caption += f"**{E_ICSS} اصدار التليثون  ↫** `{version.__version__}\n`"
+        icss_caption += f"**{E_ICSS} اصدار اڪسس ↫** `{icssversion}`\n"
+        icss_caption += f"**{E_ICSS} اصدار البايثون ↫** `{python_version()}\n`"
         #        icss_caption += f"**{EMOJI} مدة التشغيل ↫** `{uptime}\n`"
-        icss_caption += f"**{EMOJI} المستخدم ↫** {mention}\n"
-        icss_caption += f"**{EMOJI} مطور السورس ↫** [اضغط هنا](t.me/rruuurr) 𓆰.\n"
+        icss_caption += f"**{E_ICSS} المستخدم ↫** {mention}\n"
+        icss_caption += f"**{E_ICSS} مطور السورس ↫** [اضغط هنا](t.me/rruuurr) 𓆰.\n"
         icss_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐢𝐜𝐬𝐬ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
         await icss.client.send_file(
             icss.chat_id, ICSS_IMG, caption=icss_caption, reply_to=reply_to_id
@@ -37,13 +39,13 @@ async def icssalive(icss):
     else:
         await edit_or_reply(
             icss,
-            f"**{CUSTOM_ALIVE_TEXT}**\n\n"
-            f"**{EMOJI} قاعدة البيانات ↫**  `{check_sgnirts}`\n"
-            f"**{EMOJI} اصدار التليثون  ↫** `{version.__version__}\n`"
-            f"**{EMOJI} اصدار اڪسس ↫** `{catversion}`\n"
-            f"**{EMOJI} اصدار البايثون  ↫** `{python_version()}\n`"
-            f"**{EMOJI} مدة التشغيل ↫** `{uptime}\n`"
-            f"**{EMOJI} المستخدم ↫** {mention}\n",
+            f"**{ICSS_TEXT}**\n\n"
+            f"**{E_ICSS} قاعدة البيانات ↫**  `{check_sgnirts}`\n"
+            f"**{E_ICSS} اصدار التليثون  ↫** `{version.__version__}\n`"
+            f"**{E_ICSS} اصدار اڪسس ↫** `{icssversion}`\n"
+            f"**{E_ICSS} اصدار البايثون  ↫** `{python_version()}\n`"
+            f"**{E_ICSS} مدة التشغيل ↫** `{uptime}\n`"
+            f"**{E_ICSS} المستخدم ↫** {mention}\n",
         )
 
 
@@ -56,7 +58,7 @@ async def icssalive(icss):
     reply_to_id = await reply_id(icss)
     icss_caption = f"𓆩 𝑾𝑬𝑳𝑪𝑶𝑴𝑬 𝑻𝑶 𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺 𓆪\n"
     icss_caption += f"**  - اصدار التليثون ↫** `{version.__version__}\n`"
-    icss_caption += f"**  - اصدار اكسس ↫** `{catversion}`\n"
+    icss_caption += f"**  - اصدار اكسس ↫** `{icssversion}`\n"
     icss_caption += f"**  - اصدار البايثون ↫** `{python_version()}\n`"
     icss_caption += f"**  - المستخدم ↫** {mention}\n"
     results = await bot.inline_query(
@@ -67,7 +69,6 @@ async def icssalive(icss):
 
 
 def check_data_base_heal_th():
-    # https://stackoverflow.com/a/41961968
     is_database_working = False
     output = "لم يتم تعيين قاعدة بيانات"
     if not Config.DB_URI:
@@ -75,7 +76,6 @@ def check_data_base_heal_th():
     from userbot.plugins.sql_helper import SESSION
 
     try:
-        # to check database we will execute raw query
         SESSION.execute("SELECT 1")
     except Exception as e:
         output = f"❌ {str(e)}"
