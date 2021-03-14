6f84bac1d846729db8e5042925d4bb1268c0a00a
ANL0KE <79422815+ANL0KE@users.noreply.github.com>
2021-03-14 18:40:26 +0000

    pylint: auto fixes

--- userbot/plugins/scramble.py
+++ userbot/plugins/scramble.py
@@ -5,6 +5,7 @@ from userbot.utils import admin_cmd
 
 # ===================================================== #
 
+
 @icssbot.on(admin_cmd(pattern=r"scramble(\s+[\S\s]+|$)"))
 async def scramble_message(i):
     reply_message = await i.get_reply_message()
@@ -14,14 +15,16 @@ async def scramble_message(i):
     text = " ".join(scrambled)
     await i.edit(text)
 
+
 # ===================================================== #
 
-def scramble_word(word):
-    if len(word) < 4:                      
-        return word                        
 
-    first_letter = word[0]                 
-    last_letter = word[-1]                 
+def scramble_word(word):
+    if len(word) < 4:
+        return word
+
+    first_letter = word[0]
+    last_letter = word[-1]
     middle_letters = list(word[1:-1])
     random.shuffle(middle_letters)
 
