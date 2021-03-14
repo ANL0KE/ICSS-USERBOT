f5077fbe368e8380cacce51b742457227ada835c
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-14 16:17:19 +0300

    ICSS

---
+++ userbot/plugins/meme.py
@@ -0,0 +1,243 @@
+import asyncio
+
+
+@icssbot.on(admin_cmd(outgoing=True, pattern="^\:/$"))
+@icssbot.on(sudo_cmd(pattern="^\:/$", allow_sudo=True))
+async def kek(keks):
+    keks = await edit_or_reply(keks, ":\\")
+    uio = ["/", "\\"]
+    for i in range(15):
+        await asyncio.sleep(0.5)
+        txt = ":" + uio[i % 2]
+        await keks.edit(txt)
+
+
+@icssbot.on(admin_cmd(outgoing=True, pattern="^\-_-$"))
+@icssbot.on(sudo_cmd(pattern="^\-_-$", allow_sudo=True))
+async def lol(lel):
+    lel = await edit_or_reply(lel, "-__-")
+    okay = "-__-"
+    for _ in range(15):
+        await asyncio.sleep(0.5)
+        okay = okay[:-1] + "_-"
+        await lel.edit(okay)
+
+
+@icssbot.on(admin_cmd(outgoing=True, pattern="^\;_;$"))
+@icssbot.on(sudo_cmd(pattern="^\;_;$", allow_sudo=True))
+async def fun(e):
+    e = await edit_or_reply(e, ";__;")
+    t = ";__;"
+    for _ in range(15):
+        await asyncio.sleep(0.5)
+        t = t[:-1] + "_;"
+        await e.edit(t)
+
+
+@icssbot.on(admin_cmd(outgoing=True, pattern="oof$"))
+@icssbot.on(sudo_cmd(pattern="oof$", allow_sudo=True))
+async def Oof(e):
+    t = "Oof"
+    catevent = await edit_or_reply(e, t)
+    for _ in range(15):
+        await asyncio.sleep(0.5)
+        t = t[:-1] + "of"
+        await catevent.edit(t)
+
+
+@icssbot.on(admin_cmd(outgoing=True, pattern="اكتب (.*)"))
+@icssbot.on(sudo_cmd(pattern="اكتب (.*)", allow_sudo=True))
+async def typewriter(typew):
+    message = typew.pattern_match.group(1)
+    sleep_time = 0.2
+    typing_symbol = "|"
+    old_text = ""
+    typew = await edit_or_reply(typew, typing_symbol)
+    await asyncio.sleep(sleep_time)
+    for character in message:
+        old_text = old_text + "" + character
+        typing_text = old_text + "" + typing_symbol
+        await typew.edit(typing_text)
+        await asyncio.sleep(sleep_time)
+        await typew.edit(old_text)
+        await asyncio.sleep(sleep_time)
+
+
+@icssbot.on(admin_cmd(pattern="repeat (\d*) (.*)", outgoing=True))
+@icssbot.on(sudo_cmd(pattern="repeat (\d*) (.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    icss = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 1)
+    message = icss[1]
+    count = int(cat[0])
+    repmessage = (f"{message} ") * count
+    await asyncio.wait([event.respond(repmessage)])
+    await event.delete()
+
+
+@icssbot.on(admin_cmd(pattern=f"meme", outgoing=True))
+@icssbot.on(sudo_cmd(pattern=f"meme", allow_sudo=True))
+async def meme(event):
+    memeVar = event.text
+    sleepValue = 0.5
+    memeVar = memeVar[6:]
+    if not memeVar:
+        memeVar = "✈️"
+    event = await edit_or_reply(event, "-------------" + memeVar)
+    await asyncio.sleep(sleepValue)
+    await event.edit("------------" + memeVar + "-")
+    await asyncio.sleep(sleepValue)
+    await event.edit("-----------" + memeVar + "--")
+    await asyncio.sleep(sleepValue)
+    await event.edit("----------" + memeVar + "---")
+    await asyncio.sleep(sleepValue)
+    await event.edit("---------" + memeVar + "----")
+    await asyncio.sleep(sleepValue)
+    await event.edit("--------" + memeVar + "-----")
+    await asyncio.sleep(sleepValue)
+    await event.edit("-------" + memeVar + "------")
+    await asyncio.sleep(sleepValue)
+    await event.edit("------" + memeVar + "-------")
+    await asyncio.sleep(sleepValue)
+    await event.edit("-----" + memeVar + "--------")
+    await asyncio.sleep(sleepValue)
+    await event.edit("----" + memeVar + "---------")
+    await asyncio.sleep(sleepValue)
+    await event.edit("---" + memeVar + "----------")
+    await asyncio.sleep(sleepValue)
+    await event.edit("--" + memeVar + "-----------")
+    await asyncio.sleep(sleepValue)
+    await event.edit("-" + memeVar + "------------")
+    await asyncio.sleep(sleepValue)
+    await event.edit(memeVar + "-------------")
+    await asyncio.sleep(sleepValue)
+    await event.edit("-------------" + memeVar)
+    await asyncio.sleep(sleepValue)
+    await event.edit("------------" + memeVar + "-")
+    await asyncio.sleep(sleepValue)
+    await event.edit("-----------" + memeVar + "--")
+    await asyncio.sleep(sleepValue)
+    await event.edit("----------" + memeVar + "---")
+    await asyncio.sleep(sleepValue)
+    await event.edit("---------" + memeVar + "----")
+    await asyncio.sleep(sleepValue)
+    await event.edit("--------" + memeVar + "-----")
+    await asyncio.sleep(sleepValue)
+    await event.edit("-------" + memeVar + "------")
+    await asyncio.sleep(sleepValue)
+    await event.edit("------" + memeVar + "-------")
+    await asyncio.sleep(sleepValue)
+    await event.edit("-----" + memeVar + "--------")
+    await asyncio.sleep(sleepValue)
+    await event.edit("----" + memeVar + "---------")
+    await asyncio.sleep(sleepValue)
+    await event.edit("---" + memeVar + "----------")
+    await asyncio.sleep(sleepValue)
+    await event.edit("--" + memeVar + "-----------")
+    await asyncio.sleep(sleepValue)
+    await event.edit("-" + memeVar + "------------")
+    await asyncio.sleep(sleepValue)
+    await event.edit(memeVar + "-------------")
+    await asyncio.sleep(sleepValue)
+    await event.edit(memeVar)
+
+
+@icssbot.on(admin_cmd(pattern=f"give", outgoing=True))
+@icssbot.on(sudo_cmd(pattern=f"give", allow_sudo=True))
+async def give(event):
+    if event.fwd_from:
+        return
+    giveVar = event.text
+    sleepValue = 0.5
+    lp = giveVar[6:]
+    if not lp:
+        lp = " 🍭"
+    event = await edit_or_reply(event, lp + "        ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(lp + lp + "       ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(lp + lp + lp + "      ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(lp + lp + lp + lp + "     ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(lp + lp + lp + lp + lp + "    ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(lp + lp + lp + lp + lp + lp + "   ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(lp + lp + lp + lp + lp + lp + lp + "  ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(lp + lp + lp + lp + lp + lp + lp + lp + " ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(lp + lp + lp + lp + lp + lp + lp + lp + lp)
+    await asyncio.sleep(sleepValue)
+    await event.edit(lp + "        ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(lp + lp + "       ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(lp + lp + lp + "      ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(lp + lp + lp + lp + "     ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(lp + lp + lp + lp + lp + "    ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(lp + lp + lp + lp + lp + lp + "   ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(lp + lp + lp + lp + lp + lp + lp + "  ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(lp + lp + lp + lp + lp + lp + lp + lp + " ")
+    await asyncio.sleep(sleepValue)
+    await event.edit(lp + lp + lp + lp + lp + lp + lp + lp + lp)
+
+
+@icssbot.on(admin_cmd(pattern=f"sadmin$", outgoing=True))
+@icssbot.on(sudo_cmd(pattern=f"sadmin$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_ttl = range(13)
+    event = await edit_or_reply(event, "sadmin")
+    animation_chars = [
+        "@aaaaaaaaaaaaadddddddddddddmmmmmmmmmmmmmiiiiiiiiiiiiinnnnnnnnnnnnn",
+        "@aaaaaaaaaaaaddddddddddddmmmmmmmmmmmmiiiiiiiiiiiinnnnnnnnnnnn",
+        "@aaaaaaaaaaadddddddddddmmmmmmmmmmmiiiiiiiiiiinnnnnnnnnnn",
+        "@aaaaaaaaaaddddddddddmmmmmmmmmmiiiiiiiiiinnnnnnnnnn",
+        "@aaaaaaaaadddddddddmmmmmmmmmiiiiiiiiinnnnnnnnn",
+        "@aaaaaaaaddddddddmmmmmmmmiiiiiiiinnnnnnnn",
+        "@aaaaaaadddddddmmmmmmmiiiiiiinnnnnnn",
+        "@aaaaaaddddddmmmmmmiiiiiinnnnnn",
+        "@aaaaadddddmmmmmiiiiinnnnn",
+        "@aaaaddddmmmmiiiinnnn",
+        "@aaadddmmmiiinnn",
+        "@aaddmmiinn",
+        "@admin",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(1)
+        await event.edit(animation_chars[i % 13])
+
+
+CMD_HELP.update(
+    {
+        "meme": "**Plugin : **`meme`\
+        \n\n**Commands :**\
+        \n  •  `:/`\
+        \n  •  `-_-`\
+        \n  •  `;_;`\
+        \n  •  `.oof`\
+        \n\n**Functions :**\
+        \n__The above four commands are animation commands__\
+        \n\n**Commands :**\
+        \n  •  `.meme`\
+        \n  •  `.give`\
+        \n\n**Functions :**\
+        \n__The above two commands are animation memes meme by default takes ✈️ and give by default takes 🍭__\
+        \n\n**Syntax : **`.repeat count message`\
+        \n**Function : **try out and check Yourself `.repeat 5 hello`\
+        \n\n**Syntax :** `.type`\
+        \n**Function : **Just a small command to make your keyboard become a typewriter!\
+        \n\n**Syntax :** `.sadmin`\
+        \n**Function : **Fun animation of @admin!\
+        "
+    }
+)
