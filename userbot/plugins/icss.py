8f9eb3dbc907aa93d8c0b81cedb288dad2e1360e
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-15 03:21:34 +0300

    ICSS

---
+++ userbot/plugins/icss.py
@@ -0,0 +1,23 @@
+icss = [
+" ___ ____ ____ ____",
+"|_ _/ ___/ ___/ ___|",
+" | | |   \___ \___ \ ",
+" | | |___ ___) |__) |",
+"|___\____|____/____/"
+]
+@icssbot.on(admin_cmd(pattern="icss", outgoing=True))
+async def kimo(ics):
+  kim = icss
+  return await eor(ics, kim)
+
+tosh = [
+"🔱 | 𝗻𝗮𝗺𝗲 :",
+"🔱 | 𝗳𝗿𝗼𝗺 :",
+"🔱 | 𝗮𝗴𝗲 :",
+"🔱 | 𝗤𝘂𝗼𝘁𝗲 :",
+" 𝒀𝑶𝑼 𝑭𝑶𝑹𝑮𝑶𝑻 𝑯𝑶𝑾 𝑻𝑶 𝑳𝑶𝑽𝑬. 𝑨𝑮𝑨𝑰𝑵"
+]
+@icssbot.on(admin_cmd(pattern="Poo", outgoing=True))
+async def poo(pio):
+  th = tosh
+  return await eor(pio, th)
