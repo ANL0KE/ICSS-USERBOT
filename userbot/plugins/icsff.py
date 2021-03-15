f01500d578b231bc0ce45e1ea679d05d85caf4d1
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-15 18:51:42 +0300

    ICSS

--- userbot/plugins/icsff.py
+++ userbot/plugins/icsff.py
@@ -1,8 +1,12 @@
 # icss fonts
 
+icsff1 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+icsff2 = ['𝙖','𝙗','𝙘','𝙙','𝙚','𝙛','𝙜','𝙝','𝙄','𝙟','𝙠','𝙡','𝙢','𝙣','𝙤','𝙥','𝙦','𝙧','𝙨','𝙩','𝙪','𝙫','𝙬','𝙭','𝙮','𝙯']
+
+
 
 @icssbot.on(admin_cmd(pattern="ho ?(.*)"))
-async def icsf9(ics):
+async def icsf2(ics):
 
     kim = ics.pattern_match.group(1)
     if not kim:
@@ -14,6 +18,6 @@ async def icsf9(ics):
     string = "".join(kim).lower()
     for normiecharacter in string:
         if normiecharacter in icsf1:
-            wcharacter = icsf9[icsf1.index(normiecharacter)]
+            wcharacter = icsf2[icsf1.index(normiecharacter)]
             string = string.replace(normiecharacter, wcharacter)
     await ics.edit(string)
