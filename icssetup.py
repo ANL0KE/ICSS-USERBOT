ebe48bcb54f4cb79c179b6a91395d50be3b40f46
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-15 16:34:07 +0300

    ICSS

---
+++ icssetup.py
@@ -0,0 +1,21 @@
+# 𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺 - 𝑺𝑺𝑻𝑹𝑰𝑵𝑮_𝑺𝑬𝑺𝑺𝑰𝑶𝑵
+ 
+from telethon.sync import TelegramClient as tc
+from telethon.sessions import StringSession as ss
+
+print("𓆩 SOURCE ICSS -  STRING SESSION 𓆪")
+print("")
+
+APP_ID = int(input("⌔∮ ENTER APP ID HERE - "))
+API_HASH = input("⌔∮ ENTER API HASH HERE - ")
+
+with tc(ss(), APP_ID, API_HASH) as client:
+    ics = client.send_message("me", client.session.save())
+    ics.reply(
+        "⌔∮ هذا هو كود التيرمكس الخاص بك.\n⌔∮ المطور - @rruuurr. ")
+    print("")
+    print("")
+    print("⌔∮ Below is the STRING_SESSION. You can also find it in your Telegram Saved Messages.")
+    print("")
+    print("")
+    print(client.session.save())
