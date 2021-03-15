8a80e2525eca5a9995a1487064f1b6c1769be93c
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-15 04:07:05 +0300

    ICSS

---
+++ userbot/plugins/tm_gif4.py
@@ -0,0 +1,36 @@
+"""
+©icss : @rruuurr
+  - Tumblr Gif
+  - Tumblr Gif = 4
+"""
+
+
+import time
+
+from . import reply_id
+
+ICS_IMG = "https://telegra.ph/file/dccf11fffd3abda4a4f29.mp4"
+ICSS_TEXT = "𓆩 𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺 - 𝑪𝑼𝑻𝑬 𝑮𝑰𝑭 𓆪"
+
+
+@icssbot.on(admin_cmd(outgoing=True, pattern="ك4$"))
+@icssbot.on(sudo_cmd(pattern="ك4$", allow_sudo=True))
+async def tmgif(lon):
+    if lon.fwd_from:
+        return
+    reply_to_id = await reply_id(lon)
+    if ICS_IMG:
+        ics_c = f"**{CUSTOM_ICSS_TEXT}**\n"
+        ics_c += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐢𝐜𝐬𝐬ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+        ics_c += f"**↫ المتـحركه الرابعه 𓆰.**"
+        await lon.client.send_file(
+            lon.chat_id, ICS_IMG, caption=ics_c, reply_to=reply_to_id
+        )
+        await lon.delete()
+    else:
+        await edit_or_reply(
+            lon,
+            f"**{CUSTOM_ICSS_TEXT}**\n"
+            f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐢𝐜𝐬𝐬ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+            f"**↫ المتـحركه الرابعه 𓆰.**",
+        )
