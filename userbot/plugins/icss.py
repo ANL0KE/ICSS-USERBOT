7feb0e4f58363a5fd1aedc3d994439a867a30fab
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-15 11:44:54 +0300

    ICSS

--- userbot/plugins/icss.py
+++ userbot/plugins/icss.py
@@ -1,25 +1,25 @@
-icss = [
+icss = (
     " ___ ____ ____ ____",
     "|_ _/ ___/ ___/ ___|",
     " | | |   \___ \___ \ ",
     " | | |___ ___) |__) |",
     "|___\____|____/____/",
-]
+)
 
-tosh = [
+tosh = (
     "🔱 | 𝗻𝗮𝗺𝗲 :",
     "🔱 | 𝗳𝗿𝗼𝗺 :",
     "🔱 | 𝗮𝗴𝗲 :",
     "🔱 | 𝗤𝘂𝗼𝘁𝗲 :",
     " 𝒀𝑶𝑼 𝑭𝑶𝑹𝑮𝑶𝑻 𝑯𝑶𝑾 𝑻𝑶 𝑳𝑶𝑽𝑬. 𝑨𝑮𝑨𝑰𝑵",
-]
+)
 
 
-@icssbot.on(admin_cmd(pattern="icss", outgoing=True))
+@icssbot.on(admin_cmd(pattern=r"icss$"))
 async def kimo(ics):
     await edit_or_reply(ics, icss)
 
 
-@icssbot.on(admin_cmd(pattern="Poo", outgoing=True))
+@icssbot.on(admin_cmd(pattern=r"poo$"))
 async def poo(pio):
     await edit_or_reply(pio, tosh)
