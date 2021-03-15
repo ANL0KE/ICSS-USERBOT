dcaaf42004dccf581e98b0b8cb30d469656cdaf2
ANL0KE <79422815+ANL0KE@users.noreply.github.com>
2021-03-15 14:35:52 +0000

    pylint: auto fixes

--- userbot/plugins/icsff.py
+++ userbot/plugins/icsff.py
@@ -1,6 +1,5 @@
 # icss fonts
 
-from icsfonts import icsfont
 
 @icssbot.on(admin_cmd(pattern="ho ?(.*)"))
 async def icsf9(ics):
@@ -8,14 +7,13 @@ async def icsf9(ics):
     kim = ics.pattern_match.group(1)
     if not kim:
         get = await ics.get_reply_message()
-        kim = get.text   
+        kim = get.text
     if not kim:
         await ics.edit("What I am Supposed to Weebify? Please Give Text Sir")
         return
-    string = ''.join(kim).lower()
+    string = "".join(kim).lower()
     for normiecharacter in string:
         if normiecharacter in normiefont:
             wcharacter = icsf9[icsf1.index(normiecharacter)]
             string = string.replace(normiecharacter, wcharacter)
     await ics.edit(string)
-    
