1dd5797a491796e92a5c32d72870072f7b24a8dd
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-15 10:31:14 +0300

    ICSS

---
+++ userbot/plugins/heg.py
@@ -0,0 +1,24 @@
+import time
+
+from . import(
+    ALIVE_NAME, 
+    StartTime, 
+    get_readable_time, 
+    mention, 
+    reply_id
+)
+
+DEFULTUSER = ALIVE_NAME or "IcssBot"
+ICSST = " اهلا بك عزيزي في بوت اكسس "
+
+
+@icssbot.on(admin_cmd(outgoing=True, pattern="مده$"))
+@icssbot.on(sudo_cmd(pattern="مده", allow_sudo=True)) 
+async def tim(lon):
+    if lon.fwd_from: 
+       return
+    icsid = await reply_id(icsid) 
+    icst = await get_readable_time((time.() - StartTime)) 
+    await edit_or_reply(
+         lon, f"**{ICSST}**\n\n ⌔∮ المستخدم: {mention} \n⌔∮ المده: {icst}
+    )
