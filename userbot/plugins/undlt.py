e643eb22b74992440c4f014fd556843c7b432b4a
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-15 06:24:47 +0300

    ICSS

---
+++ userbot/plugins/undlt.py
@@ -0,0 +1,39 @@
+import asyncio
+
+
+@icssbot.on(admin_cmd(pattern="undlt ?(.*)"))
+@icssbot.on(sudo_cmd(pattern="undlt ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.pattern_match.group(1):
+        try:
+            ics = int(event.pattern_match.group(1))
+            input_str = ics
+        except Exception:
+            input_str = 5
+    else:
+        input_str = 5
+    c = await event.get_chat()
+    if c.admin_rights or c.creator:
+        a = await event.client.get_admin_log(
+            event.chat_id, limit=input_str, search="", edit=False, delete=True
+        )
+        for i in a:
+            await event.reply(i.original.action.message)
+    else:
+        event = await edit_or_reply(
+            event, "`You need administrative permissions in order to do this command`"
+        )
+        await asyncio.sleep(3)
+        await event.delete()
+
+
+CMD_HELP.update(
+    {
+        "undlt": "**Plugin :** `undlt`\
+        \n**Syntax : **`.undlt <count>`\
+        \n**Usage: **Fetches last <count> number of deleted messages and sends you(you must be admin in that group)  \
+"
+    }
+)
