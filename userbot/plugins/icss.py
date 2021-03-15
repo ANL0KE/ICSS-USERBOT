894aaeddf44884ba2890bf2ab1781d9277de19a2
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-15 11:00:35 +0300

    ICSS

--- userbot/plugins/icss.py
+++ userbot/plugins/icss.py
@@ -10,7 +10,7 @@ icss = [
 @icssbot.on(admin_cmd(pattern="icss", outgoing=True))
 async def kimo(ics):
     kim = icss
-    return await edit(ics, kim)
+    return await ics.edit(kim)
 
 
 tosh = [
@@ -25,4 +25,4 @@ tosh = [
 @icssbot.on(admin_cmd(pattern="Poo", outgoing=True))
 async def poo(pio):
     th = tosh
-    return await edit(pio, th)
+    return await pio.edit(th)
