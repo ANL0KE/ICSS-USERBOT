7a77f707d4e6ffba6c67046abbb3732046c678ec
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-16 10:12:05 +0300

    ICSS

--- userbot/plugins/hi.py
+++ userbot/plugins/hi.py
@@ -2,17 +2,16 @@ from . import ALIVE_NAME, BOT_USERNAME, mention
 
 DEFULTUSER = ALIVE_NAME or "ICSS"
 
-Hi = (
-    f"⌔∮ 𝑰𝑪𝑺𝑺 𝑯𝑨𝑺 𝑫𝑬𝑷𝑳𝑶𝒀𝑬𝑫\n"
-    f"➖➖➖➖➖➖➖➖➖\n"
-    f"- 𝑼𝒔𝒆𝒓𝑵𝒂𝒎𝒆 : {mention}\n"
-    f"- 𝑨𝒔𝒔𝒊𝒔𝒕𝒂𝒏𝒕 : {BOT_USERNAME}\n"
-    f"➖➖➖➖➖➖➖➖➖\n"
-    f"- 𝑺𝒖𝒑𝒑𝒐𝒓𝒕: @rruuurr\n"
-    f"➖➖➖➖➖➖➖➖➖",
-)
-
 
 @icssbot.on(admin_cmd(pattern="هها"))
 async def icss(hehe):
-    await edit_or_reply(hehe, Hi)
+    await edit_or_reply(
+        hehe, Hi
+        f"⌔∮ 𝑰𝑪𝑺𝑺 𝑯𝑨𝑺 𝑫𝑬𝑷𝑳𝑶𝒀𝑬𝑫\n"
+        f"➖➖➖➖➖➖➖➖➖\n"
+        f"- 𝑼𝒔𝒆𝒓𝑵𝒂𝒎𝒆 : {mention}\n"
+        f"- 𝑨𝒔𝒔𝒊𝒔𝒕𝒂𝒏𝒕 : {BOT_USERNAME}\n"
+        f"➖➖➖➖➖➖➖➖➖\n"
+        f"- 𝑺𝒖𝒑𝒑𝒐𝒓𝒕: @rruuurr\n"
+        f"➖➖➖➖➖➖➖➖➖",
+    )
