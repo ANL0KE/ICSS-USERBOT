eefb32d34082e6ef2f8c257910bbb1153ae3a039
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-16 09:28:54 +0300

    ICSS

--- userbot/plugins/IcssBot.py
+++ userbot/plugins/IcssBot.py
@@ -1,8 +1,8 @@
-from . import ICSS_NAME
+from . import ALIVE_NAME
 
 Tg = Config.TG_BOT_USERNAME
-IC = ICSS_NAME
-DF = ICSS_NAME if ICSS_NAME else "Icss User"
+IC = ALIVE_NAME
+DF = IC if IC else "Icss User"
 
 
 @icssbot.on(admin_cmd(pattern="هها"))
@@ -10,7 +10,7 @@ async def icss(dev):
     await dev.edit(
         "⌔∮ 𝑰𝑪𝑺𝑺 𝑯𝑨𝑺 𝑩𝑬𝑵 DEPLOYED\n"
         "➖➖➖➖➖➖➖➖➖\n"
-        "- مستخدم اكسس : {mention}\n"
+        "- مستخدم اكسس : {IC}\n"
         "- البوت الخاص بك : {Tg}\n"
         "➖➖➖➖➖➖➖➖➖\n"
         "- مطور السورس : @rruuurr\n"
