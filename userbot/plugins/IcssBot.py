204d780903036fe3cf38c1088c6213fa105779fe
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-16 12:47:11 +0300

    ICSS

--- userbot/plugins/IcssBot.py
+++ userbot/plugins/IcssBot.py
@@ -1,16 +1,17 @@
 # Icss - Userbot
 
 
-ICSR = (
-    " 𓆩 𝑾𝑬𝑳𝑪𝑶𝑴𝑬 𝑻𝑶 𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺 𓆪 \n",
-    "𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n",
-    "⌔∮ UserName :- {mention}\n",
-    "⌔∮ Source Dev :- [Click Here](t.me/rruuurr)\n",
-    "⌔∮ Repo :- [Click Here](https://github.com/ANL0KE/ICSS)\n",
-)
+from . import mention
 
 
-@icssbot.on(admin_cmd(pattern="repo (.*)"))
-@icssbot.on(sudo_cmd(pattern="repo (.*)", allow_sudo=True))
+@icssbot.on(admin_cmd(pattern="repo$"))
+@icssbot.on(sudo_cmd(pattern="repo$", allow_sudo=True))
 async def icsrepo(icsp):
-    await edit_or_reply(icsp, "{ICSR}")
+    await edit_or_reply(
+        icsp, 
+        f" 𓆩 𝑾𝑬𝑳𝑪𝑶𝑴𝑬 𝑻𝑶 𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺 𓆪 \n"
+        f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+        f"⌔∮ UserName :- {mention}\n"
+        f"⌔∮ Source Dev :- [Click Here](t.me/rruuurr)\n"
+        f"⌔∮ Repo :- [Click Here](https://github.com/ANL0KE/ICSS)\n",
+    )
