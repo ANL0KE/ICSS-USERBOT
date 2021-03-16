2cd8f96cf673adcd00259eb73f5c6b0e8110219f
ANL0KE <79422815+ANL0KE@users.noreply.github.com>
2021-03-16 06:40:30 +0000

    pylint: auto fixes

--- userbot/plugins/Hi.py
+++ userbot/plugins/Hi.py
@@ -1,13 +1,10 @@
-from . import mention
-
-
 @icssbot.on(admin_cmd(pattern="هها"))
 async def icss(dev):
     await dev.edit(
         "⌔∮ 𝑰𝑪𝑺𝑺 𝑯𝑨𝑺 𝑫𝑬𝑷𝑳𝑶𝒀𝑬𝑫\n"
         "➖➖➖➖➖➖➖➖➖\n"
         "- 𝑼𝒔𝒆𝒓𝑵𝒂𝒎𝒆 : {mention}\n"
-        "- 𝑨𝒔𝒔𝒊𝒔𝒕𝒂𝒏𝒕 : {Config.TG_BOT_USERNAME}\n" 
+        "- 𝑨𝒔𝒔𝒊𝒔𝒕𝒂𝒏𝒕 : {Config.TG_BOT_USERNAME}\n"
         "➖➖➖➖➖➖➖➖➖\n"
         "- 𝑺𝒖𝒑𝒑𝒐𝒓𝒕: @rruuurr\n"
         "➖➖➖➖➖➖➖➖➖"
