60d0d725489e05cb92b9b9918ad78876597f2b08
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-18 13:09:27 +0300

    - Kimo

--- userbot/plugins/IcssBot.py
+++ userbot/plugins/IcssBot.py
@@ -9,9 +9,17 @@ from . import mention
 async def icsrepo(icsp):
     await edit_or_reply(
         icsp,
-        f" 𓆩 𝑾𝑬𝑳𝑪𝑶𝑴𝑬 𝑻𝑶 𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺 𓆪 \n"
+        f"⌔∮ 𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺 - 𝑹𝑬𝑷𝑶 𓆪 \n"
         f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
-        f"⌔∮ UserName :- {mention}\n"
-        f"⌔∮ Source Dev :- [Click Here](t.me/rruuurr)\n"
-        f"⌔∮ Repo :- [Click Here](https://github.com/ANL0KE/ICSS)\n",
+        f"- 𝑺𝑶𝑼𝑹𝑪𝑬 𝑫𝑬𝑽 ⪼ [𝑪𝑳𝑰𝑪𝑲 𝑯𝑬𝑹𝑬](t.me/rruuurr)\n"
+        f"- 𝑺𝑶𝑼𝑹𝑪𝑬 𝑹𝑬𝑷𝑶 ⪼ [𝑪𝑳𝑰𝑪𝑲 𝑯𝑬𝑹𝑬](https://github.com/ANL0KE/ICSS)\n",
+    )
+
+@icssbot.on(admin_cmd(pattern="رابط السورس$"))
+@icssbot.on(sudo_cmd(pattern="رابط السورس$", allow_sudo=True))
+async def icsrepo(icsp):
+    await edit_or_reply(
+        icsp,
+        f"⌔∮ عزيزي {mention} ⇱\n"
+        f"⌔∮ رابط سورس اكسس ↫ [هنا](https://github.com/ANL0KE/ICSS) ⇲",
     )
