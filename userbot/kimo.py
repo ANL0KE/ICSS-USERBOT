26ca061e6220bd1fef3dd409260da58c60479b71
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-18 14:01:13 +0300

    Ok

--- userbot/kimo.py
+++ userbot/kimo.py
@@ -4,5 +4,5 @@ USERID = Config.OWNER_ID
 ALIVE_NAME = Config.ALIVE_NAME
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Icss Userbot"
 ICSBOT = Config.TG_BOT_USERNAME
-DEV = "https://t.me/rruuurr"
+DEV = "@rruuurr"
 icsme = f"[{DEFAULTUSER}](tg://user?id={USERID})"
