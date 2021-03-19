23cf17700d8c5b77f3d5cb570f9bdfe46530ea4a
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-19 14:52:46 +0300

    Ok

--- userbot/plugins/Test2.py
+++ userbot/plugins/Test2.py
@@ -6,12 +6,12 @@ from .IcssGif import *
 from userbot import bot
 @bot.on(events.NewMessage(pattern=("/alive|/start")))
 async def awake(event):
-  LEGENDX = f"ʜᴇʟʟᴏ ᴛʜɪs ɪs  {ICSBOT}\n\n"
-  LEGENDX += "ᴀʟʟ sʏsᴛᴇᴍ ɪs ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ\n\n"
-  LEGENDX += f"{BOT} ᴏs : 3.0 ʟᴀsᴛᴇsᴛ\n\n"
-  LEGENDX += f"ᴍʏ ᴍᴀsᴛᴇʀ @{bot.me.username} ☺️\n\n"
-  LEGENDX += "ғᴜʟʟʏ ᴜᴘᴅᴀᴛᴇᴅ ʙᴏᴛ\n\n"
-  LEGENDX += "ᴛᴇʟᴇᴛʜᴏɴ : 1.20 LATEST\n\n"
-  LEGENDX += "ᴛʜᴀɴᴋs ғᴏʀ ᴄʜᴇᴄᴋɪɴɢ ᴍᴇ"
+  ics = f"ʜᴇʟʟᴏ ᴛʜɪs ɪs  {ICSBOT}\n\n"
+  ics += "ᴀʟʟ sʏsᴛᴇᴍ ɪs ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ\n\n"
+  ics += f"{BOT} ᴏs : 3.0 ʟᴀsᴛᴇsᴛ\n\n"
+  ics += f"ᴍʏ ᴍᴀsᴛᴇʀ @{bot.me.username} ☺️\n\n"
+  ics += "ғᴜʟʟʏ ᴜᴘᴅᴀᴛᴇᴅ ʙᴏᴛ\n\n"
+  ics += "ᴛᴇʟᴇᴛʜᴏɴ : 1.20 LATEST\n\n"
+  ics += "ᴛʜᴀɴᴋs ғᴏʀ ᴄʜᴇᴄᴋɪɴɢ ᴍᴇ"
   BUTTON = [[Button.url("𝙼𝙰𝚂𝚃𝙴𝚁", f"https://t.me/{bot.me.username}"), Button.url(f"{ICSBOT} 𝚁𝙴𝙿𝙾", "https://github.com/ANL0KE/ICSS")]]
-  await bot.send_file(event.chat_id, PHOTO, caption=LEGENDX,  buttons=BUTTON)
+  await bot.send_file(event.chat_id, kut_gif, caption=ics,  buttons=BUTTON)
