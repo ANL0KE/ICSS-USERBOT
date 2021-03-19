61bdfac9dd189df6de8840f9446321ac2c4f41cf
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-19 18:29:50 +0300

    - ok

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -243,7 +243,7 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
     async def on_plug_in_callback_query_handler(event):
         if event.query.user_id == bot.uid:
-            await event.edit("**⌔∮ تم اغلاق القائمه**")
+            await event.edit("**⌔∮ menu closed**")
         else:
             reply_pop_up_alert = "⌔∮ Please get your own icssbot, and don't use mine! Join @rruuurr help "
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
