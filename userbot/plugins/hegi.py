f510dae94493417e50ec677bec326a034f248139
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-19 14:02:27 +0300

    - ICSS

---
+++ userbot/plugins/hegi.py
@@ -0,0 +1,25 @@
+import asyncio
+import random
+
+Ulodya = [
+   "𓄂"
+   "⇲"
+   "𖦼"
+   "❒"
+   "༕"
+   "༗"
+   ""
+   "༗"
+   "⌭"
+]
+
+@icss.on(
+    icss_cmd(
+       pattern="هاا", outgoing=True
+    )
+)
+async def icss(ics):
+   Ulo = random.choics(Ulodya)
+   await icss.edit("وجع انتظر")
+   await asyncio.sleep(3)
+   await edit_or_reply("{Ulo}")
