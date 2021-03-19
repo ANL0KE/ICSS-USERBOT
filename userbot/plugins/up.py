b23040373b1de428a9e01fa01cf33a11fe6aa79a
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-19 06:44:15 +0300

    Ok

--- userbot/plugins/up.py
+++ userbot/plugins/up.py
@@ -129,8 +129,12 @@ async def update(event, repo, ups_rem, ac_br):
     return
 
 
-@icssbot.on(admin_cmd(outgoing=True, pattern=r"تحديث($| (الان|البوت))"))
-@icssbot.on(sudo_cmd(pattern="تحديث($| (الان|البوت))", allow_sudo=True))
+@icssbot.on(
+    admin_cmd(outgoing=True, pattern=r"تحديث($| (الان|البوت))")
+)
+@icssbot.on(
+    sudo_cmd(pattern="تحديث($| (الان|البوت))", allow_sudo=True) 
+)
 async def upstream(event):
     "بالنسبة لأمر التحديث ، تحقق مما إذا كان بوت اكسس محدثًا ، أو قم بالتحديث إذا تم بتحديثه"
     conf = event.pattern_match.group(1).strip()
