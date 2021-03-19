36f4c299f088cd025630ad1d0e562106f08f85c5
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-19 07:14:08 +0300

    Ok

---
+++ userbot/plugins/Test2.py
@@ -0,0 +1,33 @@
+
+from . import reply_id, icsgif1, icstext
+
+
+@icssbot.on(
+    admin_cmd(
+       outgoing=True, pattern="احا$"
+    )
+)
+@icssbot.on(
+    sudo_cmd(
+       pattern="احا$", allow_sudo=True
+    )
+)
+async def kutgif(icss):
+    if icss.fwd_from:
+        return
+    reply_to_id = await reply_id(icss)
+    if icsgif1:
+        icss_caption = f"**{icstext}**\n"
+        icss_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐢𝐜𝐬𝐬ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+        icss_caption += f"**↫ المتـحركه الاولى 𓆰.**"
+        await icss.client.send_file(
+            icss.chat_id, ICSS_IMG, caption=icss_caption, reply_to=reply_to_id
+        )
+        await icss.delete()
+    else:
+        await edit_or_reply(
+            icss,
+            f"**{icstext}**\n"
+            f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐢𝐜𝐬𝐬ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+            f"**↫ المتـحركه الاولى 𓆰.**",
+        )
