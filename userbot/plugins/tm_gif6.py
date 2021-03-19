b51de95b74176d2be2a0635e9c7e815b6c90796c
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-19 08:25:17 +0300

    - ICSS

--- userbot/plugins/tm_gif6.py
+++ userbot/plugins/tm_gif6.py
@@ -5,29 +5,27 @@
 """
 
 from . import reply_id
-
-ICS_IMG = "https://telegra.ph/file/7fa9b1905df5840c7c77a.mp4"
-ICSS_TEXT = "𓆩𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺 - 𝑻𝑼𝑴𝑩𝑳𝑹 𝑮𝑰𝑭𓆪"
+from .IcssGif import *
 
 
-@icssbot.on(admin_cmd(outgoing=True, pattern="ت6$"))
-@icssbot.on(sudo_cmd(pattern="ت6$", allow_sudo=True))
+@icssbot.on(
+    admin_cmd(
+       outgoing=True, pattern="ت6$"
+    )
+)
+@icssbot.on(
+    sudo_cmd(
+       pattern="ت6$", allow_sudo=True
+    )
+)
 async def tmgif(kimo):
     if kimo.fwd_from:
         return
     reply_to_id = await reply_id(kimo)
-    if ICS_IMG:
-        ics_c = f"**{ICSS_TEXT}**\n"
-        ics_c += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐢𝐜𝐬𝐬ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
-        ics_c += f"**↫ المتـحركه السادسه 𓆰.**"
+    if tm_gif6:
+        tumc = f"**{TMTE}**\n"
+        tumc += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐢𝐜𝐬𝐬ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+        tumc += f"**↫ المتـحركه السادسه 𓆰.**"
         await kimo.client.send_file(
-            kimo.chat_id, ICS_IMG, caption=ics_c, reply_to=reply_to_id
-        )
-        await kimo.delete()
-    else:
-        await edit_or_reply(
-            kimo,
-            f"**{ICSS_TEXT}**\n"
-            f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐢𝐜𝐬𝐬ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
-            f"**↫ المتـحركه السادسه 𓆰.**",
+            kimo.chat_id, tm_gif6, caption=tumc, reply_to=reply_to_id
         )
