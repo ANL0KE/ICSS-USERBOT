c485d481d7f33de9f2613388a201200208bd6659
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-20 18:20:00 +0300

    - XO

--- userbot/plugins/init.py
+++ userbot/plugins/init.py
@@ -1,23 +1,9 @@
+#    IcssBot - UserBot
+#    (c) @rruuurr
+
+
 from userbot import CMD_HELP
 from userbot.Config import Config
-
-
-@icssbot.on(
-    icss_cmd(
-       pattern="stat$"
-    )
-)
-async def stats(event):
-    if event.fwd_from:
-        return
-    ahyes = await tgbot.get_me()
-    botusername = ahyes.username
-    noob = "stats"
-    if event.reply_to_msg_id:
-        await event.get_reply_message()
-    tap = await bot.inline_query(botusername, noob)
-    await tap[0].click(event.chat_id)
-    await event.delete()
 
 
 @icssbot.on(
@@ -25,6 +11,7 @@ async def stats(event):
        pattern="اكس او$"
     )
 )
+
 async def gamez(event):
     if event.fwd_from:
         return
@@ -33,39 +20,5 @@ async def gamez(event):
     if event.reply_to_msg_id:
         await event.get_reply_message()
     tap = await bot.inline_query(botusername, noob)
-    await tap[0].click(event.chat_id)
-    await event.delete()
-
-
-@icssbot.on(
-    icss_cmd(
-       pattern="همسه$"
-    )
-)
-async def wspr(event):
-    if event.fwd_from:
-        return
-    wwwspr = event.pattern_match.group(1)
-    botusername = "@whisperBot"
-    if event.reply_to_msg_id:
-        await event.get_reply_message()
-    tap = await bot.inline_query(botusername, wwwspr)
-    await tap[0].click(event.chat_id)
-    await event.delete()
-
-
-@icssbot.on(
-    icss_cmd(
-       pattern="mod$"
-    )
-)
-async def mod(event):
-    if event.fwd_from:
-        return
-    modr = event.pattern_match.group(1)
-    botusername = "@PremiumAppBot"
-    if event.reply_to_msg_id:
-        await event.get_reply_message()
-    tap = await bot.inline_query(botusername, modr)
     await tap[0].click(event.chat_id)
     await event.delete()
