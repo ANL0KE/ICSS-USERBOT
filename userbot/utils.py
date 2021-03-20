d229bce2909249b9638233f84e2292d0b82f3a66
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-20 18:57:15 +0300

    Hmm

--- userbot/utils.py
+++ userbot/utils.py
@@ -73,7 +73,7 @@ def load_module(shortname):
         sys.modules["uniborg.util"] = userbot.utils
         mod.borg = bot
         mod.icssbot = bot
-        mod.ics_cmd = admin_cmd, bot
+        mod.ics_cmd = admin_cmd and bot
         mod.ics_cmd = ics_cmd       
         mod.icss = bot
         mod.icss = icss
