6561b60887920471e354578c48e7f44e75526ebd
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-26 13:20:03 +0300

    - Ok

--- userbot/plugins/heg.py
+++ userbot/plugins/heg.py
@@ -1,18 +1,22 @@
 import time
 
-from . import ALIVE_NAME, StartTime, get_readable_time, mention, reply_id
+from . import StartTime, get_readable_time, mention, rid
 
-DEFULTUSER = ALIVE_NAME or "IcssBot"
-ICSST = " اهلا بك عزيزي في بوت اكسس "
-
-
-@icssbot.on(admin_cmd(pattern="مده", outgoing=True))
-@icssbot.on(sudo_cmd(pattern="مده", allow_sudo=True))
+@icss(
+    icss_cmd(
+       pattern="مده", outgoing=True
+    )
+)
+@icss(
+    sudo_cmd(
+       pattern="مده", allow_sudo=True
+    )
+)
 async def tim(lon):
     if lon.fwd_from:
         return
-    await reply_id(lon)
+    await rid(lon)
     icst = await get_readable_time((time.time() - StartTime))
-    await edit_or_reply(
+    await eor(
         lon, f"**{ICSST}**\n\n ⌔∮ المستخدم: {mention} \n⌔∮ المده: {icst}"
     )
