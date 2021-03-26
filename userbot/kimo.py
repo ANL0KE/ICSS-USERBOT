1327711df5a1571797cbe078774a91df90634141
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-26 23:56:33 +0300

    - Ok

--- userbot/kimo.py
+++ userbot/kimo.py
@@ -1,7 +1,6 @@
-from . import bot
 from .Config import Config
 
-ICSI = Config.OWNER_ID or bot.uid
+ICSI = Config.OWNER_ID
 ICSN = Config.ALIVE_NAME
 ICSD = str(ICSN) if ICSN else "Icss Userbot"
 ICSB = Config.TG_BOT_USERNAME
