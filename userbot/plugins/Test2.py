c7cbfb5ad2bf23c0d9d03b4d180fa10a56c60dcd
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-26 12:26:12 +0300

    + Tg

--- userbot/plugins/Test2.py
+++ userbot/plugins/Test2.py
@@ -2,16 +2,19 @@
 from telethon import events, Button, custom
 import re, os
 from . import ICSBOT
-from .IcssGif import *
 from userbot import bot
-@bot.on(events.NewMessage(pattern=("/alive|/start")))
+@tgbot.on(events.NewMessage(pattern=("/alive|/start")))
 async def awake(event):
-  ics = f"ʜᴇʟʟᴏ ᴛʜɪs ɪs  {ICSBOT}\n\n"
-  ics += "ᴀʟʟ sʏsᴛᴇᴍ ɪs ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ\n\n"
-  ics += f"{ICSBOT} ᴏs : 3.0 ʟᴀsᴛᴇsᴛ\n\n"
-  ics += f"ᴍʏ ᴍᴀsᴛᴇʀ @{bot.me.username} ☺️\n\n"
-  ics += "ғᴜʟʟʏ ᴜᴘᴅᴀᴛᴇᴅ ʙᴏᴛ\n\n"
-  ics += "ᴛᴇʟᴇᴛʜᴏɴ : 1.20 LATEST\n\n"
-  ics += "ᴛʜᴀɴᴋs ғᴏʀ ᴄʜᴇᴄᴋɪɴɢ ᴍᴇ"
-  BUTTON = [[Button.url("𝙼𝙰𝚂𝚃𝙴𝚁", f"https://t.me/{bot.me.username}"), Button.url(f"{ICSBOT} 𝚁𝙴𝙿𝙾", "https://github.com/ANL0KE/ICSS")]]
-  await bot.send_file(event.chat_id, kut_gif, caption=ics,  buttons=BUTTON)
+ ics = "hii\n"
+ ics += f"heloo {ICSBOT}"
+  BUTTON = [[Button.url("𝙼𝙰𝚂𝚃𝙴𝚁", f"https://t.me/{bot.me.username}"), Button.url(f"{ICSBOT} 𝚁𝙴𝙿𝙾", "https://github.com/ANL0KE/ICSS-USERBOT")]]
+  await tgbot.send_file(
+      event.chat_id, caption=ics,  buttons=BUTTON
+  )
+
+@tgbot.on(events.NewMessage(pattern=("/repo|#repo")))
+async def repo(event):
+  await tgbot.send_message(
+      event.chat, "thnx", buttons=[[Button.url("⚜️ icsd ⚜️", "https://github.com/ANL0KE/ICSS-USERBOT")]]
+  )
+
