0c545f4a7498d808b23b806ce765125672623f41
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-26 12:53:22 +0300

    - ICSD

--- userbot/kimo.py
+++ userbot/kimo.py
@@ -5,5 +5,5 @@ ICSI = Config.OWNER_ID or bot.uid
 ICSN = Config.ALIVE_NAME
 ICSD = str(ICSN) if ICSN else "Icss Userbot"
 ICSB = Config.TG_BOT_USERNAME
-DEV = "@rruuurr"
-icsme = f"[{ICSD}](tg://user?id={ICSI})"
+ICSE = "@rruuurr"
+ICSM = f"[{ICSD}](tg://user?id={ICSI})"
