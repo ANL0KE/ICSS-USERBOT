276a00045dc333e89fb4d2a9e45ca940ab71614c
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-26 12:51:56 +0300

    - Ok

--- userbot/kimo.py
+++ userbot/kimo.py
@@ -1,9 +1,9 @@
 from . import bot
 from .Config import Config
 
-USERID = Config.OWNER_ID or bot.uid
-ALIVE_NAME = Config.ALIVE_NAME
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Icss Userbot"
-ICSBOT = Config.TG_BOT_USERNAME
+ICSI = Config.OWNER_ID or bot.uid
+ICSN = Config.ALIVE_NAME
+ICSD = str(ICSN) if ICSN else "Icss Userbot"
+ICSB = Config.TG_BOT_USERNAME
 DEV = "@rruuurr"
-icsme = f"[{DEFAULTUSER}](tg://user?id={USERID})"
+icsme = f"[{ICSD}](tg://user?id={ICSI})"
