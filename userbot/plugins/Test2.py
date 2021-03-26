9a0275c04b427924adf0301af48f8b6cfbee4536
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-27 00:18:36 +0300

    - Ok

--- userbot/plugins/Test2.py
+++ userbot/plugins/Test2.py
@@ -8,9 +8,9 @@ async def awake(event):
   ics = "hii\n"
   ics += f"heloo {ICSBOT}"
   BUTTON = [[Button.url("𝙼𝙰𝚂𝚃𝙴𝚁", f"https://t.me/{bot.me.username}"), Button.url(f"{ICSBOT} 𝚁𝙴𝙿𝙾", "https://github.com/ANL0KE/ICSS-USERBOT")]]
-  await event.send_file(event.chat_id, caption=ics, buttons=BUTTON)
+  await tgbot.send_file(event.chat_id, caption=ics, buttons=BUTTON)
 
 @tgbot.on(events.NewMessage(pattern=("/repo|#repo")))
 async def repo(event):
-  await event.send_message(event.chat, "thnx", buttons=[[Button.url("⚜️ icsd ⚜️", "https://github.com/ANL0KE/ICSS-USERBOT")]])
+  await tgbot.send_message(event.chat, "thnx", buttons=[[Button.url("⚜️ icsd ⚜️", "https://github.com/ANL0KE/ICSS-USERBOT")]])
 
