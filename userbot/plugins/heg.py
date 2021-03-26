5912e72c4852d4f71b3fbc9c01461779c9043124
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-26 13:24:48 +0300

    - Ok

--- userbot/plugins/heg.py
+++ userbot/plugins/heg.py
@@ -1,6 +1,7 @@
 import time
 
-from . import StartTime, get_readable_time, mention, rd
+from . import StartTime, mention, rd
+from . import get_readable_time as grt
 
 @icss(
     icss_cmd(
@@ -16,7 +17,7 @@ async def tim(lon):
     if lon.fwd_from:
         return
     await rd(lon)
-    icst = await get_readable_time((time.time() - StartTime))
+    icst = await grt((time.time() - StartTime))
     await eor(
         lon, f"**{ICSST}**\n\n ⌔∮ المستخدم: {mention} \n⌔∮ المده: {icst}"
     )
