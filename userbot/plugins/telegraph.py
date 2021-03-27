436c47192777ccd92ce3741282f60942c615a6b4
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-27 14:04:48 +0300

    - Ok

--- userbot/plugins/telegraph.py
+++ userbot/plugins/telegraph.py
@@ -6,71 +6,81 @@
 """
 
 
+# telegraph utils for Source Icss
+
 import os
-from datetime import datetime as ict
+from datetime import datetime
 
 from PIL import Image
 from telegraph import Telegraph, exceptions, upload_file
 
-from . import BOTLOG_CHATID
+from . import BOTLOG_CHATID, mention
 
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=Config.TELEGRAPH_SHORT_NAME)
 auth_url = r["auth_url"]
 
 
-@icssbot.on(admin_cmd(pattern="تلكراف (ميديا|نص) ?(.*)"))
+@icssbot.on(
+    icss_cmd(
+       pattern="تلكراف (ميديا|نص) ?(.*)"
+    )
+)
 @icssbot.on(sudo_cmd(pattern="تلكراف(ميديا|نص) ?(.*)", allow_sudo=True))
-@icssbot.on(admin_cmd(pattern="tg(m|t) ?(.*)"))
+@icssbot.on(
+    icss_cmd(
+       pattern="tg(m|t) ?(.*)"
+    )
+)
 @icssbot.on(sudo_cmd(pattern="tg(m|t) ?(.*)", allow_sudo=True))
-async def _(event):
-    if event.fwd_from:
+async def _(tosh):
+    if tosh.fwd_from:
         return
-    icse = await edit_or_reply(event, "** ⪼ جاري المعالجه ༗...**")
+    ice = await eor(tosh, "** ⪼ جاري المعالجه ༗...**")
     if not os.path.isdir(Config.TEMP_DIR):
         os.makedirs(Config.TEMP_DIR)
         #     if BOTLOG:
-        await event.client.send_message(
+        await tosh.client.send_message(
             BOTLOG_CHATID,
-            "Created New Telegraph account {} for the current session. \n**Do not give this url to anyone, even if they say they are from Telegram!**".format(
+            "**⌔∮ تم إنشاء حساب Telegraph جديد {} للدورة الحالية‌‌** \n**⌔∮ لا تعطي عنوان url هذا لأي شخص**".format(
                 auth_url
             ),
         )
-    optional_title = event.pattern_match.group(2)
-    if event.reply_to_msg_id:
-        start = ict.now()
-        r_message = await event.get_reply_message()
-        input_str = event.pattern_match.group(1)
+    optional_title = tosh.pattern_match.group(2)
+    if tosh.reply_to_msg_id:
+        start = datetime.now()
+        r_message = await tosh.get_reply_message()
+        input_str = tosh.pattern_match.group(1)
         if input_str in ["ميديا", "m"]:
-            downloaded_file_name = await event.client.download_media(
+            downloaded_file_name = await tosh.client.download_media(
                 r_message, Config.TEMP_DIR
             )
-            end = ict.now()
+            end = datetime.now()
             ms = (end - start).seconds
-            await icse.edit(
+            await ice.edit(
                 f"** ⪼ تم تحميل {downloaded_file_name} في وقت {ms} ثانيه.**"
             )
             if downloaded_file_name.endswith((".webp")):
                 resize_image(downloaded_file_name)
             try:
-                start = ict.now()
+                start = datetime.now()
                 media_urls = upload_file(downloaded_file_name)
             except exceptions.TelegraphException as exc:
-                await icse.edit("**⌔∮ حدث خطأ : **" + str(exc))
+                await ice.edit("**⌔∮ خطا : **" + str(exc))
                 os.remove(downloaded_file_name)
             else:
-                end = ict.now()
+                end = datetime.now()
                 ms_two = (end - start).seconds
                 os.remove(downloaded_file_name)
-                await icse.edit(
-                    "**الرابط : **[اضغط هنا](https://telegra.ph{})\
-                    \n**الوقت : **`{} ثانيه.`".format(
-                        media_urls[0], (ms + ms_two)
+                await ice.edit(
+                    "**⌔∮ الرابط : **[اضغط هنا](https://telegra.ph{})\
+                    \n**⌔∮ الوقت : **`{} ثانيه.` \n**⌔∮ تم التحميل بواسطه :** {}".format(
+                        media_urls[0], (ms + ms_two), (mention)
                     ),
                     link_preview=True,
                 )
         elif input_str in ["نص", "t"]:
-            user_object = await event.client.get_entity(r_message.sender_id)
+            user_object = await tosh.client.get_entity(r_message.sender_id)
             title_of_page = user_object.first_name  # + " " + user_object.last_name
             # apparently, all Users do not have last_name field
             if optional_title:
@@ -79,7 +89,7 @@ async def _(event):
             if r_message.media:
                 if page_content != "":
                     title_of_page = page_content
-                downloaded_file_name = await event.client.download_media(
+                downloaded_file_name = await tosh.client.download_media(
                     r_message, Config.TEMP_DIR
                 )
                 m_list = None
@@ -90,16 +100,16 @@ async def _(event):
                 os.remove(downloaded_file_name)
             page_content = page_content.replace("\n", "<br>")
             response = telegraph.create_page(title_of_page, html_content=page_content)
-            end = ict.now()
+            end = datetime.now()
             ms = (end - start).seconds
-            ics = f"https://telegra.ph/{response['path']}"
-            await icse.edit(
-                f"**الرابط : ** [اضغط هنا]({ics})\
+            kimo = f"https://telegra.ph/{response['path']}"
+            await ice.edit(
+                f"**الرابط : ** [اضغط هنا]({kimo})\
                  \n**الوقت : **`{ms} ثانيه.`",
                 link_preview=True,
             )
     else:
-        await icse.edit(
+        await ice.edit(
             "قم بالرد على رسالة للحصول على رابط تللكراف دائم.",
         )
 
