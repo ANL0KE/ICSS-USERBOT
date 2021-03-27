75d2fb7cbaff588c72c0078b55e6b7c02f42e231
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-27 12:07:54 +0300

    - Ok

--- userbot/plugins/Test2.py
+++ userbot/plugins/Test2.py
@@ -1,16 +1,75 @@
 
 from telethon import events, Button, custom
 import re, os
+from userbot.Config import Config
 from . import ICSBOT
 
-@bot.on(events.NewMessage(pattern=("/alive|/start")))
+LOAD_MYBOT = os.environ.get("LOAD_MYBOT", "True")
+BOT_PIC = Config.ALIVE_PIC if ALIVE_PIC else None
+OWNER_ID = Config.OWNER_ID
+
+@bot.on(events.NewMessage(pattern=("/alive|")))
 async def awake(event):
   ics = "hii\n"
   ics += f"heloo {ICSBOT}"
   BUTTON = [[Button.url("𝙼𝙰𝚂𝚃𝙴𝚁", f"https://t.me/{bot.me.username}"), Button.url(f"{ICSBOT} 𝚁𝙴𝙿𝙾", "https://github.com/ANL0KE/ICSS-USERBOT")]]
   await bot.send_file(event.chat_id, caption=ics, buttons=BUTTON)
 
-@bot.on(events.NewMessage(pattern=("/repo|#repo")))
+
+@tgbot.on(events.NewMessage(pattern=("/repo|#repo")))
 async def repo(event):
   await bot.send_message(event.chat, "thnx", buttons=[[Button.url("⚜️ icsd ⚜️", "https://github.com/ANL0KE/ICSS-USERBOT")]])
 
+
+@tgbot.on(events.NewMessage(pattern="^/start"))  # pylint: disable=oof
+async def start_all(event):
+    if event.chat_id == OWNER_ID:
+        return
+    target = event.sender_id
+    if present_in_userbase(target):
+        pass
+    else:
+        try:
+            add_to_userbase(target)
+        except BaseException:
+            pass
+    if LOAD_MYBOT == "False":
+        if BOT_PIC:
+            await tgbot.send_file(event.chat_id,
+                                  BOT_PIC,
+                                  caption=startotherdis,
+                                  buttons=[
+                                      (Button.inline(
+                                          "What can I do here?",
+                                          data="wew"))]
+                                  )
+        else:
+            await tgbot.send_message(event.chat_id,
+                                     startotherdis,
+                                     buttons=[
+                                         (Button.inline(
+                                             "What can I do here?",
+                                             data="wew"))]
+                                     )
+    elif LOAD_MYBOT == "True":
+        if BOT_PIC:
+            await tgbot.send_file(event.chat_id,
+                                  BOT_PIC,
+                                  caption=startotherena,
+                                  buttons=[
+                                      [Button.url(
+                                          "TeleBot", url="https://github.com/xditya/TeleBot")],
+                                      [Button.inline(
+                                          "Whats this?", data="telebot")]
+                                  ]
+                                  )
+        else:
+            await tgbot.send_message(event.chat_id,
+                                     startotherena,
+                                     buttons=[
+                                         [Button.url(
+                                             "TeleBot", url="https://github.com/xditya/TeleBot")],
+                                         [Button.inline(
+                                             "Whats this?", data="telebot")]
+                                     ]
+                                     )
