62d3323f7da6a16c8672003e56bd9dedf30fcab1
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-27 11:49:17 +0300

    - Ok

--- userbot/plugins/Test2.py
+++ userbot/plugins/Test2.py
@@ -3,14 +3,14 @@ from telethon import events, Button, custom
 import re, os
 from . import ICSBOT
 
-@tgbot.on(events.NewMessage(pattern=("/alive|/start")))
+@bot.on(events.NewMessage(pattern=("/alive|/start")))
 async def awake(event):
   ics = "hii\n"
   ics += f"heloo {ICSBOT}"
   BUTTON = [[Button.url("𝙼𝙰𝚂𝚃𝙴𝚁", f"https://t.me/{bot.me.username}"), Button.url(f"{ICSBOT} 𝚁𝙴𝙿𝙾", "https://github.com/ANL0KE/ICSS-USERBOT")]]
-  await tgbot.send_file(event.chat_id, caption=ics, buttons=BUTTON)
+  await bot.send_file(event.chat_id, caption=ics, buttons=BUTTON)
 
-@tgbot.on(events.NewMessage(pattern=("/repo|#repo")))
+@bot.on(events.NewMessage(pattern=("/repo|#repo")))
 async def repo(event):
-  await tgbot.send_message(event.chat, "thnx", buttons=[[Button.url("⚜️ icsd ⚜️", "https://github.com/ANL0KE/ICSS-USERBOT")]])
+  await bot.send_message(event.chat, "thnx", buttons=[[Button.url("⚜️ icsd ⚜️", "https://github.com/ANL0KE/ICSS-USERBOT")]])
 
