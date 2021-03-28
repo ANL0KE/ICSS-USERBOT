d561b5025e0218d89de3ffd467ea493467277515
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-28 22:01:24 +0300

    - Ok

--- userbot/plugins/kut_gif.py
+++ userbot/plugins/kut_gif.py
@@ -1,19 +1,29 @@
 # KutGif for icss by: @rruuurr
 
-from . import reply_id
+from . import reply_id as rd
 from .IcssGif import *
 
 
-@icssbot.on(admin_cmd(outgoing=True, pattern="ك1$"))
-@icssbot.on(sudo_cmd(pattern="ك1$", allow_sudo=True))
+@icssbot.on(
+    icss_cmd(
+       outgoing=True, 
+       pattern="ك1$",
+    )
+)
+@icssbot.on(
+    sudo_cmd(
+       pattern="ك1$", 
+       allow_sudo=True, 
+    )
+)
 async def kutgif(icss):
     if icss.fwd_from:
         return
-    reply_to_id = await reply_id(icss)
+    Ti = await rd(icss)
     if kut_gif:
         icss_caption = f"**{KUTTE}**\n"
         icss_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐢𝐜𝐬𝐬ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
         icss_caption += f"**↫ المتـحركه الاولى 𓆰.**"
         await icss.client.send_file(
-            icss.chat_id, kut_gif, caption=icss_caption, reply_to=reply_to_id
+            icss.chat_id, kut_gif, caption=icss_caption, reply_to=Ti
         )
