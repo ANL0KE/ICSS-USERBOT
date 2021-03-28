13b5ff07e7846c8191f7442354df274328c0665e
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-28 22:15:11 +0300

    - Ok

---
+++ userbot/plugins/tosha/__init__.py
@@ -0,0 +1,20 @@
+# Icss - Userbot
+# من هنا يتم تحميل المتحركات
+# gif loaded from here
+
+KUTTE = "𓆩 𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺 - 𝑪𝑼𝑻𝑬 𝑮𝑰𝑭 𓆪"
+kut_gif = "https://telegra.ph/file/eea08954a3f302fc9add0.mp4"
+kut_gif2 = "https://telegra.ph/file/834d5db098aacb76f91dd.mp4" 
+kut_gif3 = "https://telegra.ph/file/78185fe07950955f07e74.mp4"
+kut_gif4 = "https://telegra.ph/file/dccf11fffd3abda4a4f29.mp4"
+kut_gif5 = "https://telegra.ph/file/48c31877954d9fb53ee1f.mp4"
+kut_gif6 = "https://telegra.ph/file/c7fe5c6b44a09754ad5c8.mp4"
+kut_gif7 = "https://telegra.ph/file/787f0c1d759a8b4faa0fc.mp4"
+TMTE = "𓆩𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺 - 𝑻𝑼𝑴𝑩𝑳𝑹 𝑮𝑰𝑭𓆪"
+tm_gif = "https://telegra.ph/file/0f57dc58339a693ae3db2.mp4"
+tm_gif2 = "https://telegra.ph/file/6e707dc14cb918cd765fb.mp4"
+tm_gif3 = "https://telegra.ph/file/ecf8aa1a881954a9ed196.mp4"
+tm_gif4 = "https://telegra.ph/file/dccf11fffd3abda4a4f29.mp4"
+tm_gif5 = "https://telegra.ph/file/4e6d2784767f22c707883.mp4"
+tm_gif6 = "https://telegra.ph/file/7fa9b1905df5840c7c77a.mp4"
+tm_gif7 = "https://telegra.ph/file/6efa916e79bdf7fe3fba4.mp4"
