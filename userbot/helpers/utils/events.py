77da6db75041e9be9e139a0f42857237552eee1c
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-28 16:19:28 +0300

    - Ok

--- userbot/helpers/utils/events.py
+++ userbot/helpers/utils/events.py
@@ -1,7 +1,7 @@
 from telethon.tl.types import MessageEntityMentionName
 
 from ...Config import Config
-from ...tosh import ed
+from ...managers import edit_delete as ed
 
 
 async def reply_id(event):
