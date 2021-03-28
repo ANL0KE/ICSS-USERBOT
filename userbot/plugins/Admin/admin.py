8dcba221dd743b2975ea0e3254656e2ec7d2570e
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-28 20:55:57 +0300

    - Ok

--- userbot/plugins/Admin/admin.py
+++ userbot/plugins/Admin/admin.py
@@ -510,94 +510,6 @@ async def kick(usr):
             f"الدردشه: {usr.chat.title}(`{usr.chat_id}`)\n",
         )
 
-
-@icssbot.on(admin_cmd(pattern="تثبيت($| (.*))", command="pin"))
-@icssbot.on(sudo_cmd(pattern="تثبيت($| (.*))", command="pin", allow_sudo=True))
-@errors_handler
-async def pin(msg):
-    if msg.fwd_from:
-        return
-    if not msg.is_private:
-        await msg.get_chat()
-    to_pin = msg.reply_to_msg_id
-    if not to_pin:
-        return await edit_delete(msg, "**الرد على رسالة لتثبيتها.**", 5)
-    options = msg.pattern_match.group(1)
-    is_silent = False
-    if options == "loud":
-        is_silent = True
-    try:
-        await msg.client.pin_message(msg.chat_id, to_pin, notify=is_silent)
-    except BadRequestError:
-        return await ed(msg, NO_PERM, 5)
-    except Exception as e:
-        return await ed(msg, f"`{str(e)}`", 5)
-    await ed(msg, "**تم التثبيت بنجاح✔**", 3)
-    user = await get_user_from_id(msg.sender_id, msg)
-    if BOTLOG and not msg.is_private:
-        try:
-            await msg.client.send_message(
-                BOTLOG_CHATID,
-                "#تثبيت\n"
-                f"الادمن: [{user.first_name}](tg://user?id={user.id})\n"
-                f"الدردشه: {msg.chat.title}(`{msg.chat_id}`)\n",
-                #                 f"LOUD: {is_silent}",
-            )
-        except:
-            pass
-
-
-@icssbot.on(admin_cmd(pattern="الغاء تثبيت($| (.*))", command="unpin"))
-@icssbot.on(sudo_cmd(pattern="الغاء تثبيت($| (.*))", command="unpin", allow_sudo=True))
-@errors_handler
-async def pin(msg):
-    if msg.fwd_from:
-        return
-    if not msg.is_private:
-        await msg.get_chat()
-    to_unpin = msg.reply_to_msg_id
-    options = (msg.pattern_match.group(1)).strip()
-    if not to_unpin and options != "الكل":
-        await ed(
-            msg,
-            "**يمكنك الرد على رسالة لإلغاء تثبيتها أو استخدام .الغاء تثبيت الكل**",
-            5,
-        )
-        return
-    if to_unpin and not options:
-        try:
-            await msg.client.unpin_message(msg.chat_id, to_unpin)
-        except BadRequestError:
-            return await ed(msg, NO_PERM, 5)
-        except Exception as e:
-            return await ed(msg, f"`{str(e)}`", 5)
-    elif options == "الكل":
-        try:
-            await msg.client.unpin_message(msg.chat_id)
-        except BadRequestError:
-            return await ed(msg, NO_PERM, 5)
-        except Exception as e:
-            return await ed(msg, f"`{str(e)}`", 5)
-    else:
-        return await ed(
-            msg,
-            "** - ❝ ⌊   يمكنك الرد على رسالة لإلغاء تثبيتها أو استخدام .الغاء تثبيت الكل 𓆰.**",
-            5,
-        )
-    await ed(msg, "**تم إلغاء التثبيت بنجاح✔**", 3)
-    user = await get_user_from_id(msg.sender_id, msg)
-    if BOTLOG and not msg.is_private:
-        try:
-            await msg.client.send_message(
-                BOTLOG_CHATID,
-                "#الغاء_تثبيت\n"
-                f"**الادمن : **[{user.first_name}](tg://user?id={user.id})\n"
-                f"**الدردشه : **{msg.chat.title}(`{msg.chat_id}`)\n",
-            )
-        except:
-            pass
-
-
 CMD_HELP.update(
     {
         "admin": "**Plugin : **`admin`\
@@ -615,9 +527,6 @@ CMD_HELP.update(
         \n  •  **Usage : **Mutes the person in the chat, works on admins too.\
         \n\n  •  **Syntax : **`.unmute` <username/reply>\
         \n  •  **Usage : **Removes the person from the muted list.\
-        \n\n  •  **Syntax : **`.pin `<reply> or `.pin loud`\
-        \n  •  **Usage : **Pins the replied message in Group\
-        \n\n  •  **Syntax : **`.unpin `<reply> or `.unpin all`\
         \n  •  **Usage : **Unpins the replied message in Group\
         \n\n  •  **Syntax : **`.kick `<username/reply> \
         \n  •  **Usage : **kick the person off your chat.\
