c8cc05e618e1ee37d082dac367204e2e7f8f0ca2
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-28 20:29:46 +0300

    - Ok

---
+++ userbot/plugins/animations/__init__.py
@@ -0,0 +1,16 @@
+# Hey there
+
+from userbot.Config import Config
+
+# =================== OWNER - ANL0KE =================== #
+
+USERID = Config.OWNER_ID 
+ALIVE_NAME = Config.ALIVE_NAME
+AUTONAME = Config.AUTONAME
+DEFAULT_BIO = Config.DEFAULT_BIO
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Icss Userbot"
+BOT_USERNAME = Config.TG_BOT_USERNAME
+ICSBOT = Config.TG_BOT_USERNAME
+mention = f"[{DEFAULTUSER}](tg://user?id={USERID})"
+
+# ======================================================= #
