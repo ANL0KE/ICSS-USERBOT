60f1076590937f50010fc6574172d9c8412c2c0e
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-29 08:52:43 +0300

    __hmm__

--- userbot/plugins/Dev.py
+++ userbot/plugins/Dev.py
@@ -2,11 +2,12 @@
 #  TELE - @NIIIN2
 
 from telethon import events
-from . import tosh
+from . import tosh, reply_id
 
 @bot.on(
     events.NewMessage(pattern=("المطور"))
 )
 async def dev(kimo):
-    await bot.send_message(kimo.chat, tosh)
+    rd = await reply_id(kimo)
+    await bot.send_message(kimo.chat, tosh, reply_to=rd)
         
