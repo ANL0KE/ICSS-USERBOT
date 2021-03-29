270d531e81adbee4f50910e3b7475d3baf8dead9
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-29 20:28:06 +0300

    $

--- userbot/plugins/Dev.py
+++ userbot/plugins/Dev.py
@@ -14,5 +14,5 @@ async def dev(kimo):
     events.NewMessage(pattern=("/start"))
 )
 async def dev(kimo):
-    await tgbot.send_message(kimo.chat, f"اهلا بك - {mention}\n انا بوت مساعد للمستخدم {ALIVE_NAME} ", buttons=[[Button.url("✨ المطور ✨", "https://t.me/rruuurr")]])
+    await tgbot.send_message(kimo.chat, f"**- اني بوت مساعد للمستخدم** {mention}", buttons=[[Button.url("✨ المطور ✨", "https://t.me/rruuurr")]])
             
