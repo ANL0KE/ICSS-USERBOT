63d04ab958ba031fd11fa3c6a4fbd3e6a185cffe
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-30 00:33:37 +0300

    - Ok

--- userbot/plugins/Dev.py
+++ userbot/plugins/Dev.py
@@ -16,3 +16,16 @@ async def dev(kimo):
 async def dev(kimo):
     await tgbot.send_message(kimo.chat, f"**- اني بوت مساعد للمستخدم** {mention}", buttons=[[Button.url("✨ المطور ✨", "https://t.me/rruuurr")]])
             
+
+@tgbot.on(
+    events.NewMessage(pattern=("/ics"))
+)
+async def repo(event):
+    if event.fwd_from:
+        return
+    KIM = Config.TG_BOT_USERNAME
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    response = await tgbot.send_message(KIM, "البوت")
+    await response[0].click(event.chat_id)
+    await event.delete()
