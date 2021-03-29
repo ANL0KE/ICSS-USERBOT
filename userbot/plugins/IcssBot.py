03f0bdfe6a8a436110f284d4043e9ea935c368b4
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-29 22:37:36 +0300

    - Ok

--- userbot/plugins/IcssBot.py
+++ userbot/plugins/IcssBot.py
@@ -1,32 +1,17 @@
 #    Icss - Userbot
 #    Owner - @rruuurr
 
-from . import mention
+from . import R, K
 
-
-@icssbot.on(
-    icss_cmd(
-       pattern="repo$"
-    )
-)
-@icssbot.on(
-    sudo_cmd(
-       pattern="repo$", allow_sudo=True
-    )
-)
+@icssbot.on(icss_cmd(pattern="repo$"))
+@icssbot.on(sudo_cmd(pattern="repo$", allow_sudo=True))
 async def icsrepo(icsp):
     await eor(icsp, R)
 
 
-@icssbot.on(
-    icss_cmd(
-       pattern="رابط السورس$"
-    )
-)
-@icssbot.on(
-    sudo_cmd(
-       pattern="رابط السورس$", allow_sudo=True
-    )
-)
+@icssbot.on(icss_cmd(pattern="رابط السورس$"))
+@icssbot.on(sudo_cmd(pattern="رابط السورس$", allow_sudo=True))
 async def icsrepo(icsp):
     await eor(icsp, K)
+
+
