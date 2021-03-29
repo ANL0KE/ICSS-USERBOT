81b7f1bd45662de2792ebe82e4e34729c79a364c
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-29 17:45:35 +0300

    Update Dev.py

--- userbot/plugins/Dev.py
+++ userbot/plugins/Dev.py
@@ -2,11 +2,17 @@
 #  TELE - @NIIIN2
 
 from telethon import events, Button
-from . import tosh
+from . import tosh, mention
 
 @tgbot.on(
     events.NewMessage(pattern=("المطور"))
 )
 async def dev(kimo):
     await tgbot.send_message(kimo.chat, "مطور بوت اكسس", buttons=[[Button.url("✨ المطور ✨", "https://t.me/rruuurr")]])
-        
+   
+@tgbot.on(
+    events.NewMessage(pattern=("/start"))
+)
+async def dev(kimo):
+    await tgbot.send_message(kimo.chat, f"اهلا بك - {mention}\n انا بوت مساعد للمستخدم {ALIVE_NAME} ", buttons=[[Button.url("✨ المطور ✨", "https://t.me/rruuurr")]])
+             
