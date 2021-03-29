de483db4b9f6234aba91d69caf8eda14594d4357
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-29 08:43:47 +0300

    - Ok

---
+++ userbot/plugins/Dev.py
@@ -0,0 +1,12 @@
+#  ICSS - USERBOT
+#  TELE - @NIIIN2
+
+from telethon import events
+from . import tosh
+
+@bot.on(
+    events.NewMessage(pattern=("المطور"))
+)
+async def dev(kimo):
+    await bot.send_message(kimo, tosh)
+        
