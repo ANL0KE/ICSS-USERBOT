3cd76b0876fba598183acb0f0042c329f463696b
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-29 23:49:46 +0300

    - Hmm

--- userbot/plugins/IcssBot.py
+++ userbot/plugins/IcssBot.py
@@ -20,4 +20,44 @@ async def dev(kimo):
     )
    
 
+TOSH = f"**- اهلا بك انا {ICSB} بوت اكسس مساعد **\n**- للمستخدم** : {mention}"
+TOSH_PIC = "https://telegra.ph/file/85dc02885566034ef51a4.jpg"
+
+if Config.TG_BOT_USERNAME is not None and tgbot is not None:
+    @tgbot.on(events.InlineQuery)
+    async def inline_handler(event):
+        builder = event.builder
+        result = None
+        query = event.text
+        me = await bot.get_me()
+        if query.startswith("alive") and event.query.user_id == bot.uid:
+            buttons = [
+                [
+                    Button.url("الرابط", K),
+                    Button.url("القناة", "https://t.me/rruuurr"),
+                ]
+            ]
+            if TOSH_PIC and TOSH_PIC.endswith((".jpg", ".png", "gif", "mp4")):
+                result = builder.photo(
+                    TOSH_PIC,
+                    text=TOSH,
+                    buttons=buttons,
+                    link_preview=False
+                )
+            elif ALIVE_PHOTTO:
+                result = builder.document(
+                    TOSH_PIC,
+                    title="ICSS - USERBOT",
+                    text=TOSH,
+                    buttons=buttons,
+                    link_preview=False,
+                )
+            else:
+                result = builder.article(
+                    title="ICSS - USERBOT",
+                    text=TOSH,
+                    buttons=buttons,
+                    link_preview=False,
+                )
+            await event.answer([result] if result else None)
 
