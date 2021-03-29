d8e6fc97aead8f0d98abe30de4084a950c1086b4
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-29 17:14:43 +0300

    - Ok

---
+++ userbot/plugins/Tosh_cmd.py
@@ -0,0 +1,82 @@
+#   ICSS - USERBOT
+#   OWNER : ANL0KE
+#   TELE : @NIIIN2
+
+from .Tosh import *
+
+@icssbot.on(icss_cmd(pattern="الاوامر"))
+async def icss(tosh):
+    return await eor(tosh, A)
+
+@icssbot.on(icss_cmd(pattern="م1"))
+async def icss(tosh):
+    return await eor(tosh, B)
+
+@icssbot.on(icss_cmd(pattern="م2"))
+async def icss(tosh):
+    return await eor(tosh, C)
+
+@icssbot.on(icss_cmd(pattern="م3"))
+async def icss(tosh):
+    return await eor(tosh, D)
+
+@icssbot.on(icss_cmd(pattern="م4"))
+async def icss(tosh):
+    return await eor(tosh, E)
+
+@icssbot.on(icss_cmd(pattern="م5"))
+async def icss(tosh):
+    return await eor(tosh, F)
+
+@icssbot.on(icss_cmd(pattern="م6"))
+async def icss(tosh):
+    return await eor(tosh, G)
+
+@icssbot.on(icss_cmd(pattern="م7"))
+async def icss(tosh):
+    return await eor(tosh, H)
+
+@icssbot.on(icss_cmd(pattern="م8"))
+async def icss(tosh):
+    return await eor(tosh, I)
+
+@icssbot.on(icss_cmd(pattern="م9"))
+async def icss(tosh):
+    return await eor(tosh, J)
+
+@icssbot.on(icss_cmd(pattern="م10"))
+async def icss(tosh):
+    return await eor(tosh, K)
+
+@icssbot.on(icss_cmd(pattern="م11"))
+async def icss(tosh):
+    return await eor(tosh, L)
+
+@icssbot.on(icss_cmd(pattern="م12"))
+async def icss(tosh):
+    return await eor(tosh, M)
+
+@icssbot.on(icss_cmd(pattern="م13"))
+async def icss(tosh):
+    return await eor(tosh, N)
+
+@icssbot.on(icss_cmd(pattern="م14"))
+async def icss(tosh):
+    return await eor(tosh, O)
+
+@icssbot.on(icss_cmd(pattern="م15"))
+async def icss(tosh):
+    return await eor(tosh, P)
+
+@icssbot.on(icss_cmd(pattern="م16"))
+async def icss(tosh):
+    return await eor(tosh, Q)
+
+@icssbot.on(icss_cmd(pattern="م17"))
+async def icss(tosh):
+    return await eor(tosh, R)
+
+@icssbot.on(icss_cmd(pattern="م18"))
+async def icss(tosh):
+    return await eor(tosh, S)
+
