055582ece4b7718bfe273a422e229ccc8430f54d
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-30 07:11:53 +0300

    - Ok

--- userbot/plugins/Tosha.py
+++ userbot/plugins/Tosha.py
@@ -6,37 +6,37 @@ from .Tosh import *
 
 @icssbot.on(icss_cmd(pattern="م10"))
 async def icss(tosh):
-    return await eor(tosh, K)
+    await eor(tosh, K)
 
 @icssbot.on(icss_cmd(pattern="م11"))
 async def icss(tosh):
-    return await eor(tosh, L)
+    await eor(tosh, L)
 
 @icssbot.on(icss_cmd(pattern="م12"))
 async def icss(tosh):
-    return await eor(tosh, M)
+    await eor(tosh, M)
 
 @icssbot.on(icss_cmd(pattern="م13"))
 async def icss(tosh):
-    return await eor(tosh, N)
+    await eor(tosh, N)
 
 @icssbot.on(icss_cmd(pattern="م14"))
 async def icss(tosh):
-    return await eor(tosh, O)
+    await eor(tosh, O)
 
 @icssbot.on(icss_cmd(pattern="م15"))
 async def icss(tosh):
-    return await eor(tosh, P)
+    await eor(tosh, P)
 
 @icssbot.on(icss_cmd(pattern="م16"))
 async def icss(tosh):
-    return await eor(tosh, Q)
+    await eor(tosh, Q)
 
 @icssbot.on(icss_cmd(pattern="م17"))
 async def icss(tosh):
-    return await eor(tosh, R)
+    await eor(tosh, R)
 
 @icssbot.on(icss_cmd(pattern="م18"))
 async def icss(tosh):
-    return await eor(tosh, S)
+    await eor(tosh, S)
 
