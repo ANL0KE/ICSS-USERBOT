a6366e6b65ede99b5305d88978d341af5fe6e293
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-30 06:27:30 +0300

    - Hm

--- userbot/plugins/wspr.py
+++ userbot/plugins/wspr.py
@@ -27,3 +27,17 @@ async def kimo(lon):
         ics_c = f"اذا تريد ترسل همسه من خلال البوت الخاص بك يجب كتابه اولا #معرف_البوت ثم #secret ثم تكتب #معرف_الي_تريد_تهمسله ثم #الرساله وستضهر ايقونه وتضغط عليها وبس 🖤✨.\n"
         ics_c += f"**- قم بنسخ :**\n `{ICSB} secret @NIIIN2 الرساله`"
         await lon.client.send_file(lon.chat_id, WPIC, caption=ics_c, reply_to=ld)
+
+
+
+@bot.on(admin_cmd(pattern="wspr ?(.*)"))
+async def wspr(event):
+    if event.fwd_from:
+        return
+    wwwspr = event.pattern_match.group(1)
+    bu = "@whisperBot"
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    tap = await bot.inline_query(bu, wwwspr) 
+    await tap[0].click(event.chat_id)
+    await event.delete()
