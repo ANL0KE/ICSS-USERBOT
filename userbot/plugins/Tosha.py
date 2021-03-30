dff6dc6fbf3bf26669821df0ea9ad10a3c640320
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-30 07:10:58 +0300

    - Ok

--- userbot/plugins/Tosha.py
+++ userbot/plugins/Tosha.py
@@ -1,48 +1,8 @@
 #   ICSS - USERBOT
 #   OWNER : ANL0KE
 #   TELE : @NIIIN2
-"""
+
 from .Tosh import *
-
-@icssbot.on(icss_cmd(pattern="الاوامر"))
-async def icss(tosh):
-    return await eor(tosh, A)
-
-@icssbot.on(icss_cmd(pattern="م1"))
-async def icss(tosh):
-    return await eor(tosh, B)
-
-@icssbot.on(icss_cmd(pattern="م2"))
-async def icss(tosh):
-    return await eor(tosh, C)
-
-@icssbot.on(icss_cmd(pattern="م3"))
-async def icss(tosh):
-    return await eor(tosh, D)
-
-@icssbot.on(icss_cmd(pattern="م4"))
-async def icss(tosh):
-    return await eor(tosh, E)
-
-@icssbot.on(icss_cmd(pattern="م5"))
-async def icss(tosh):
-    return await eor(tosh, F)
-
-@icssbot.on(icss_cmd(pattern="م6"))
-async def icss(tosh):
-    return await eor(tosh, G)
-
-@icssbot.on(icss_cmd(pattern="م7"))
-async def icss(tosh):
-    return await eor(tosh, H)
-
-@icssbot.on(icss_cmd(pattern="م8"))
-async def icss(tosh):
-    return await eor(tosh, I)
-
-@icssbot.on(icss_cmd(pattern="م9"))
-async def icss(tosh):
-    return await eor(tosh, J)
 
 @icssbot.on(icss_cmd(pattern="م10"))
 async def icss(tosh):
@@ -80,4 +40,3 @@ async def icss(tosh):
 async def icss(tosh):
     return await eor(tosh, S)
 
-"""
