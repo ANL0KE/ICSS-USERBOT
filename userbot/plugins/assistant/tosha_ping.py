22d27317ced3fd7648303f8bb5035effe5c43108
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-31 17:42:59 +0300

    - Ok

---
+++ userbot/plugins/assistant/tosha_ping.py
@@ -0,0 +1,15 @@
+#   ICSS - USERBOT
+#   TELE - @NIIIN2
+
+from telethon import events
+from datetime import datetime
+
+@tgbot.on(events.NewMessage(pattern="^/ping",
+                            from_users=OWNER_ID)) 
+async def botping(event):
+    start = datetime.now()
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.reply(
+        f"⌔∮ **سرعه الاستجابه ↫** `{ms}` ** ⇲**"
+    )
