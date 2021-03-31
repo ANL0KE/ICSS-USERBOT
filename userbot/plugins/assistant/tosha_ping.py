d072fb7f8b0c15bdbbc483fab8cd4e0cef2f4791
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-31 22:33:23 +0300

    - Ok

--- userbot/plugins/assistant/tosha_ping.py
+++ userbot/plugins/assistant/tosha_ping.py
@@ -1,20 +1,16 @@
 #   ICSS - USERBOT
 #   TELE - @NIIIN2
 
-import time
 from telethon import events
-from . get_readable_time
-from .. import StartTime
+from datetime import datetime
 
-@tgbot.on(events.NewMessage(pattern="^/ping",
-                            from_users=OWNER_ID)) 
+@tgbot.on(events.NewMessage(pattern="/ping",
+                            from_users=OWNER_ID))
 async def _(event):
-    if event.fwd_from:
-        return
     start = datetime.now()
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    uptime = get_readable_time((time.time() - StartTime))
-    await tgbot.send_message(event.chat, 
-        f"⌔∮ **سرعه الاستجابه ↫** `{ms}` ** ⇲**"
+    await tgbot.send_message(
+        event.chat_id,
+        f"**Pong!!**\n `{ms}ms`",
     )
