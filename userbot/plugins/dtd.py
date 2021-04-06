b7e2808855722293802fc02776504d50b800bf88
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-06 15:01:09 +0300

    Delete dtd.py

--- userbot/plugins/dtd.py
+++
@@ -1,9 +0,0 @@
-from .Tosh import *
-
-@icssbot.on(icss_cmd(pattern="م1"))
-async def icss(tosh):
-    await eor(tosh, R)
-
-@icssbot.on(icss_cmd(pattern="م11"))
-async def icss(tosh):
-    await eor(tosh, L)
