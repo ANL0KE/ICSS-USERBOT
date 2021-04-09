846a9852b0195c0790655f1fd9cb41573bcce46f
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-09 21:19:40 +0300

    - Ok

--- userbot/plugins/animations/__init__.py
+++ userbot/plugins/animations/__init__.py
@@ -1,7 +1,7 @@
 #   ICSS - USERBOT
 #   TELE - @NIIIN2
 
-# ======================================================= #
+#  ======================================================= #
 
 from userbot.Config import Config
 
@@ -10,4 +10,4 @@ ALIVE_NAME = Config.ALIVE_NAME
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Icss Userbot"
 mention = f"[{DEFAULTUSER}](tg://user?id={USERID})"
 
-# ======================================================= #
+#  ======================================================= #
