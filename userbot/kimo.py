be964e907ed4368b332ad0b626e0b710918c7e60
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-16 06:07:37 +0300

    - Ok

--- userbot/kimo.py
+++ userbot/kimo.py
@@ -7,7 +7,7 @@ ICSN = Config.ALIVE_NAME
 ICSD = str(ICSN) if ICSN else "Icss Userbot"
 ICSB = Config.TG_BOT_USERNAME
 ICSE = "@rruuurr"
-ICSM = f"[{ICSD}](tg://user?id={ICSI})"
+TION = f"[{ICSD}](tg://user?id={ICSI})"
 ICSA = "<ⵧⵧⵧⵧⵧⵧ⫷ - ICSS ASSISTANT - ⫸ⵧⵧⵧⵧⵧⵧ>"
 ICST = "<ⵧⵧⵧⵧⵧⵧⵧⵧ⫷ - ICSS TOSHA - ⫸ⵧⵧⵧⵧⵧⵧⵧ>"
 ICSC = "<ⵧⵧⵧⵧⵧⵧ⫷ - ICSS ANIMATIONS - ⫸ⵧⵧⵧⵧⵧ>"
@@ -17,7 +17,7 @@ MSGE = (
     f"**⌔∮ تم تحديث سورس اكسس 𓄂 **\n"
     f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻 \n"
     f"**- اكتب .بنك لتحقق اذا ما كان البوت يعمل **\n"
-    f"**- المستخدم :** {ICSM} \n"
+    f"**- المستخدم :** {TION} \n"
     f"**- بوت المستخدم : {ICSB}** \n"
     f"**- للمساعده : {ICSE}**\n"
     f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻"
