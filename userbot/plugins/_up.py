5057be048a5694c3d0545fd130be95938050a4dc
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-16 04:58:32 +0300

    Update _up.py

--- userbot/plugins/_up.py
+++ userbot/plugins/_up.py
@@ -235,7 +235,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
 @bot.on(sudo_cmd(pattern="update deploy$", allow_sudo=True))
 async def upstream(event):
     event = await edit_or_reply(event, "`Pulling the catpack repo wait a sec ....`")
-    off_repo = "https://github.com/Mr-confused/catpack"
+    off_repo = "https://github.com/ANL0KE/ICSSBACK"
     os.chdir("/app")
     catcmd = f"rm -rf .git"
     try:
@@ -269,25 +269,6 @@ async def upstream(event):
     await event.edit("`Deploying userbot, please wait....`")
     await deploy(event, repo, ups_rem, ac_br, txt)
 
-
-@bot.on(admin_cmd(pattern=r"badcat$", outgoing=True))
-@bot.on(sudo_cmd(pattern=r"badcat$", allow_sudo=True))
-async def variable(var):
-    if Config.HEROKU_API_KEY is None:
-        return await edit_delete(
-            var,
-            "Set the required var in heroku to function this normally `HEROKU_API_KEY`.",
-        )
-    if Config.HEROKU_APP_NAME is not None:
-        app = Heroku.app(Config.HEROKU_APP_NAME)
-    else:
-        return await edit_delete(
-            var,
-            "Set the required var in heroku to function this normally `HEROKU_APP_NAME`.",
-        )
-    heroku_var = app.config()
-    await edit_or_reply(var, f"`Changing goodcat to badcat wait for 2-3 minutes.`")
-    heroku_var["UPSTREAM_REPO"] = "https://github.com/Jisan09/catuserbot"
 
 
 CMD_HELP.update(
