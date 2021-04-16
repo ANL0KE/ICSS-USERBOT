78b42e20f5a1118d250db460d33ffb79ece12b39
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-16 06:06:31 +0300

    - Ok

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -79,7 +79,6 @@ for name in files:
 
 
 # For admin tools
-print(ICSW)
 print(ICSL)
 path = "userbot/plugins/Admin/*.py"
 files = glob.glob(path)
@@ -98,7 +97,6 @@ for name in files:
 
 
 # for animations
-print(ICSW)
 print(ICSC)
 path = "userbot/plugins/animations/*.py"
 files = glob.glob(path)
@@ -117,7 +115,6 @@ for name in files:
 
 
 # for Gif
-print(ICSW)
 print(ICST)
 path = "userbot/plugins/tosha/*.py"
 files = glob.glob(path)
@@ -136,7 +133,6 @@ for name in files:
 
 
 # for assistant
-print(ICSW)
 print(ICSA)
 path = "userbot/plugins/assistant/*.py"
 files = glob.glob(path)
