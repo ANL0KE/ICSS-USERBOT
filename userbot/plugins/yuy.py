41a00e5e815856b86cc1481de3fbf14a069347c4
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-17 11:38:40 +0300

    - hm

--- userbot/plugins/yuy.py
+++ userbot/plugins/yuy.py
@@ -1,30 +1,24 @@
 import time
 from platform import python_version
 from telethon import version
-from telethon.errors.rpcerrorlist import ChatSendMediaForbiddenError
 from ..Config import Config
 from . import StartTime, mention, icsv, alive_1
 
+Tpic = "https://telegra.ph/file/648b0cab5d52daa8b19f6.jpg"
+kim = alive_1.format(version.__version__, icsv, python_version(), mention)
+
 @icssbot.on(
-    icss_cmd(pattern="alive$")
+    icss_cmd(pattern="alive")
 )
 async def lol(tosh):
-    Tpic = Config.ALIVE_PIC or "https://telegra.ph/file/648b0cab5d52daa8b19f6.jpg"
-    kim = (alive_1.format(
-        version.__version__,
-        icsv,
-        python_version(),
-        mention
-    )
     if Tpic:
-            await tosh.reply(kim, file=Tpic)
-            await tosh.delete()
-        except ChatSendMediaForbiddenError:
-            await eor(tosh, kim)
+        await tosh.reply(kim, file=Tpic)
+        await tosh.delete()
+        await eor(tosh, kim)
     else:
         try:
-            await bot.send_message(tosh.chat_id, file=Tpic)
-            await bot.send_message(tosh.chat_id, kim)
-            await tosh.delete()
-        except ChatSendMediaForbiddenError:
-            await eor(tosh, kim)
+           await bot.send_message(tosh.chat_id, file=Tpic)
+           await bot.send_message(tosh.chat_id, kim)
+           await tosh.delete()
+           await eor(tosh, kim)
+  
