32da01877a4741fee872bf274002b88acb50723e
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-17 11:16:58 +0300

    - hm

--- userbot/plugins/yuy.py
+++ userbot/plugins/yuy.py
@@ -1,15 +1,31 @@
 import time
-from datetime import datetime as dt
-from . import StartTime, mention
-from . import get_readable_time as grt
+from platform import python_version
+from telethon import version
+from . import StartTime, mention, icsv, get_readable_time as grt
 
 @icssbot.on(
-    icss_cmd(pattern="ختبار$")
+    icss_cmd(pattern="alive$")
 )
-async def _(tosh):
-    start = dt.now()
-    e = await eor(tosh, "⌭")
-    end = dt.now()
-    ms = (end - start).microseconds / 1000
-    grt((time.time() - StartTime))
-    await e.edit(ToK.format(ms, mention))
+async def lol(tosh):
+    pic = Config.ALIVE_PIC or "https://telegra.ph/file/648b0cab5d52daa8b19f6.jpg"
+    icsupt = await grt(time.time() - StartTime)
+    kim = (alive_1.format(
+        version.__version__,
+        icsv,
+        python_version(),
+        mention
+    )
+    if pic:
+            await tosh.reply(kim, file=pic)
+            await tosh.delete()
+        except ChatSendMediaForbiddenError:
+            await eor(tosh, kim)
+    else:
+        try:
+            await bot.send_message(tosh.chat_id, file=pic)
+            await bot.send_message(tosh.chat_id, kim)
+            await tosh.delete()
+        except ChatSendMediaForbiddenError:
+            await eor(tosh, kim)
+    if pic is None:
+        return await eor(tosh, kim)
