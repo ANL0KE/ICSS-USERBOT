9a2ed74ab6e06507af2e06b9457bef12a6ef9935
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-17 10:50:55 +0300

    - Ok

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -12,7 +12,7 @@ from platform import python_version
 from telethon import version
 from userbot.kimo import *
 
-from . import ALIVE_NAME, StartTime, get_readable_time, _alv, icsv, mention
+from . import ALIVE_NAME, StartTime, get_readable_time, icsv, mention
 from . import reply_id as rd
 
 DEFAULTUSER = ALIVE_NAME or "ICSS"
@@ -30,12 +30,31 @@ async def ica(icss):
     icsupt = await get_readable_time((time.time() - StartTime))
     _, check_sgnirts = check_data_base_heal_th()
     if ICSS_IMG:
+        ics_c = f"**{ICSS_TEXT}**\n"
+        ics_c += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻 \n"
+        ics_c += f"**{ICSEM} قاعدة البيانات ↫** `{check_sgnirts}`\n"
+        ics_c += f"**{ICSEM} اصدار التليثون  ↫** `{version.__version__}\n`"
+        ics_c += f"**{ICSEM} اصدار اڪسس ↫** `{icsv}`\n"
+        ics_c += f"**{ICSEM} اصدار البايثون ↫** `{python_version()}\n`"
+        #        ics_c += f"**{ICSEM} مدة التشغيل ↫** `{icsupt}\n`"
+        ics_c += f"**{ICSEM} المستخدم ↫** {mention}\n"
+        ics_c += f"**{ICSEM} مطور السورس ↫** [اضغط هنا](t.me/rruuurr) 𓆰.\n"
+        ics_c += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
         await icss.client.send_file(
-            icss.chat_id, ICSS_IMG, _alv, reply_to=ics_id
+            icss.chat_id, ICSS_IMG, caption=ics_c, reply_to=ics_id
         )
         await icss.delete()
     else:
-        await eor(icss, _alv)
+        await eor(
+            icss,
+            f"**{ICSS_TEXT}**\n\n"
+            f"**{ICSEM} قاعدة البيانات ↫**  `{check_sgnirts}`\n"
+            f"**{ICSEM} اصدار التليثون  ↫** `{version.__version__}\n`"
+            f"**{ICSEM} اصدار اڪسس ↫** `{icsv}`\n"
+            f"**{ICSEM} اصدار البايثون  ↫** `{python_version()}\n`"
+            f"**{ICSEM} مدة التشغيل ↫** `{icsupt}\n`"
+            f"**{ICSEM} المستخدم ↫** {mention}\n",
+        )
 
 
 def check_data_base_heal_th():
