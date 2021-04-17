30ac95caa5821897db9897c03cbd7a1fb6cc9e67
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-17 08:51:24 +0300

    - OoKk

--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -7,7 +7,7 @@ from . import ALIVE_NAME, CMD_LIST, SUDO_LIST
 from .sql_helper.globals import addgvar, gvarstatus
 
 
-@icssbot.on(admin_cmd(outgoing=True, pattern="help ?(.*)"))
+@bot.on(admin_cmd(outgoing=True, pattern="help ?(.*)"))
 async def cmd_list(event):
     if event.fwd_from:
         return
@@ -18,12 +18,14 @@ async def cmd_list(event):
     reply_to_id = await reply_id(event)
     input_str = event.pattern_match.group(1)
     if input_str == "text":
-        string = "⌔∮ مجموع {count} الاوامر الموجوده في {plugincount} الاضافات المضافه في سورس اكسس. \n\n"
+        string = (
+            "Total {count} commands found in {plugincount} plugins of catuserbot\n\n"
+        )
         catcount = 0
         plugincount = 0
         for i in sorted(CMD_LIST):
             plugincount += 1
-            string += f"⌔∮ {plugincount}) الاوامر الموجوده في الاضافه " + i + " هيه. \n"
+            string += f"{plugincount}) Commands found in Plugin " + i + " are \n"
             for iter_list in CMD_LIST[i]:
                 string += "    " + str(iter_list)
                 string += "\n"
@@ -40,16 +42,14 @@ async def cmd_list(event):
                 .get("key")
             )
             url = f"https://nekobin.com/{key}"
-            reply_text = (
-                f"**⌔∮ جميع اوامر سورس اكسس موجوده ستكون هنا - [الاوامر]({url})**"
-            )
+            reply_text = f"**All commands of the catuserbot can be seen [here]({url})**"
             await event.edit(reply_text)
             return
         await event.edit(string.format(count=catcount, plugincount=plugincount))
         return
     if input_str:
         if input_str in CMD_LIST:
-            string = "<b>⌔∮ {count} اوامر الموجوده في الاضافه {input_str}:</b>\n\n"
+            string = "<b>{count} Commands found in plugin {input_str}:</b>\n\n"
             catcount = 0
             for i in CMD_LIST[input_str]:
                 string += f"  •  <code>{i}</code>"
@@ -59,22 +59,22 @@ async def cmd_list(event):
                 string.format(count=catcount, input_str=input_str), parse_mode="HTML"
             )
         else:
-            await event.edit(input_str + " الاضافه غير صالحه ")
+            await event.edit(input_str + " is not a valid plugin!")
             await asyncio.sleep(3)
             await event.delete()
     else:
         if HELPTYPE is True:
-            help_string = f"𓆩 𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺 𝑼𝑺𝑬𝑹𝑩𝑶𝑻 𝑯𝑬𝑳𝑷𝑬𝑹 𓆪\n⌔∮ اهلا عزيزي {ALIVE_NAME} ستجد هنا جميع اضافات سورس اكسس\
-                          \n⪼ اكتب الامر `.help plugin name` للأوامر، في حالة عدم ظهور النوافذ الشفافه.\
-                          \n⪼ اكتب الامر `.info plugin name` للاستخدام من هذه الإضافات والأوامر"
+            help_string = f"Userbot Helper. Provided by {ALIVE_NAME} to reveal all the plugins\
+                          \nCheck `.help plugin name` for commands, in case popup doesn't appear.\
+                          \nCheck `.info plugin name` for usage of thoose plugins and commands"
             tgbotusername = Config.TG_BOT_USERNAME
             results = await event.client.inline_query(tgbotusername, help_string)
             await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
             await event.delete()
         else:
-            string = "<b>⌔∮ يرجى تحديد الاضافه التي تريد المساعدة فيها \
-                \n⌔∮ عدد الاضافات : </b><code>{count}</code>\
-                \n<b>⌔∮ استخدم :</b> <code>.help plugin name</code> \n\n"
+            string = "<b>Please specify which plugin do you want help for !!\
+                \nNumber of plugins : </b><code>{count}</code>\
+                \n<b>Usage:</b> <code>.help plugin name</code> \n\n"
             catcount = 0
             for i in sorted(CMD_LIST):
                 string += "◆ " + f"<code>{str(i)}</code>"
@@ -83,18 +83,18 @@ async def cmd_list(event):
             await event.edit(string.format(count=catcount), parse_mode="HTML")
 
 
-@icssbot.on(sudo_cmd(allow_sudo=True, pattern="جميع ?(.*)"))
+@bot.on(sudo_cmd(allow_sudo=True, pattern="help ?(.*)"))
 async def info(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
-    if input_str == "الاوامر":
+    if input_str == "text":
         string = "Total {count} commands found in {plugincount} sudo plugins of catuserbot\n\n"
         catcount = 0
         plugincount = 0
         for i in sorted(SUDO_LIST):
             plugincount += 1
-            string += f"⌔∮ {plugincount}) الاوامر الموجوده في الاضافه " + i + " هيه \n"
+            string += f"{plugincount}) Commands found in Plugin " + i + " are \n"
             for iter_list in SUDO_LIST[i]:
                 string += "    " + str(iter_list)
                 string += "\n"
@@ -111,9 +111,7 @@ async def info(event):
                 .get("key")
             )
             url = f"https://nekobin.com/{key}"
-            reply_text = (
-                f"**⌔∮ جميع اوامر سورس اكسس موجوده ستكون هنا فقط - [اضغط هنا]({url})**"
-            )
+            reply_text = f"All commands of the catuserbot are [here]({url})"
             await event.reply(reply_text, link_preview=False)
             return
         await event.reply(
@@ -122,7 +120,7 @@ async def info(event):
         return
     if input_str:
         if input_str in SUDO_LIST:
-            string = "<b>⌔∮ {count} الاوامر الموجوده في الاضافه {input_str}:</b>\n\n"
+            string = "<b>{count} Commands found in plugin {input_str}:</b>\n\n"
             catcount = 0
             for i in SUDO_LIST[input_str]:
                 string += f"  •  <code>{i}</code>"
@@ -132,14 +130,14 @@ async def info(event):
                 string.format(count=catcount, input_str=input_str), parse_mode="HTML"
             )
         else:
-            reply = await event.reply(input_str + " الاضافه غير صالحه ")
+            reply = await event.reply(input_str + " is not a valid plugin!")
             await asyncio.sleep(3)
             await event.delete()
             await reply.delete()
     else:
-        string = "<b>⌔∮ يرجى تحديد الاضافه التي تريد المساعدة فيها \
-            \n⌔∮ عدد الاضافات : </b><code>{count}</code>\
-            \n<b>⌔∮ استخدم :</b> <code>.help plugin name</code> \n\n"
+        string = "<b>Please specify which plugin do you want help for !!\
+            \nNumber of plugins : </b><code>{count}</code>\
+            \n<b>Usage:</b> <code>.help plugin name</code>\n\n"
         catcount = 0
         for i in sorted(SUDO_LIST):
             string += "◆ " + f"<code>{str(i)}</code>"
@@ -148,8 +146,8 @@ async def info(event):
         await event.reply(string.format(count=catcount), parse_mode="HTML")
 
 
-@icssbot.on(admin_cmd(outgoing=True, pattern="info ?(.*)"))
-@icssbot.on(sudo_cmd(pattern="info ?(.*)", allow_sudo=True))
+@bot.on(admin_cmd(outgoing=True, pattern="info ?(.*)"))
+@bot.on(sudo_cmd(pattern="info ?(.*)", allow_sudo=True))
 async def info(event):
     if event.fwd_from:
         return
@@ -176,26 +174,26 @@ async def info(event):
             await event.edit(string.format(count=catcount), parse_mode="HTML")
 
 
-@icssbot.on(admin_cmd(pattern="dc$"))
-@icssbot.on(sudo_cmd(pattern="dc$", allow_sudo=True))
+@bot.on(admin_cmd(pattern="dc$"))
+@bot.on(sudo_cmd(pattern="dc$", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     result = await event.client(functions.help.GetNearestDcRequest())
     result = (
         _format.yaml_format(result)
-        + "\n**⌔∮ قائمه مركز بيانات التليكرام :**\
-                \n⪼ DC1 : Miami FL, USA\
-                \n⪼ DC2 : Amsterdam, NL\
-                \n⪼ DC3 : Miami FL, USA\
-                \n⪼ DC4 : Amsterdam, NL\
-                \n⪼ DC5 : Singapore, SG\
+        + "\n\n**List Of Telegram Data Centres:**\
+                \nDC1 : Miami FL, USA\
+                \nDC2 : Amsterdam, NL\
+                \nDC3 : Miami FL, USA\
+                \nDC4 : Amsterdam, NL\
+                \nDC5 : Singapore, SG\
                 "
     )
     await edit_or_reply(event, result)
 
 
-@icssbot.on(admin_cmd(outgoing=True, pattern="setinline (true|false)"))
+@bot.on(admin_cmd(outgoing=True, pattern="setinline (true|false)"))
 async def _(event):
     if event.fwd_from:
         return
@@ -207,16 +205,16 @@ async def _(event):
         HELPTYPE = True
     if HELPTYPE:
         if h_type:
-            await event.edit("**⌔∮ inline mode is already enabled. **")
+            await event.edit("`inline mode is already enabled`")
         else:
             addgvar("HELPTYPE", h_type)
-            await event.edit("**⌔∮ inline mode is disabled. **")
+            await event.edit("`inline mode is disabled`")
     else:
         if h_type:
             addgvar("HELPTYPE", h_type)
-            await event.edit("**⌔∮ inline mode is enabled. **")
+            await event.edit("`inline mode is enabled`")
         else:
-            await event.edit("**⌔∮ inline mode is already disabled. **")
+            await event.edit("`inline mode is already disabled`")
 
 
 CMD_HELP.update(
