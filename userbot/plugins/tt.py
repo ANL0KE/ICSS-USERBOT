25c2d5d09588528fe1bd8e06574fee98374d0a54
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-18 19:55:45 +0300

    .

--- userbot/plugins/tt.py
+++
@@ -1,9 +0,0 @@
-Repo = "https://github.com/ANL0KE/kimo-kimo/blob/master/kimo.py"
-
-from Repo import *
-
-@icss.on(
-    icss_cmd(pattern="تيت")
-)
-async def tt(tosh):
-   await eor(tosh,Masg.format(mention, TBOT))
