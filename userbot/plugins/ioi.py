584f84ed893d996a950ec905fab50c6238de71a1
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-18 21:38:17 +0300

    Update ioi.py

--- userbot/plugins/ioi.py
+++ userbot/plugins/ioi.py
@@ -2,12 +2,10 @@ import asyncio
 
 from ..kimo import *
 
-@icss.on(
+@icssbot.on(
     icss_cmd(pattern="ببب$")
 )
 async def _(event):
-    if event.fwd_from:
-        return
     e = await eor(event, "yyy")
     chat = "@BotFather"
     async with event.client.conversation(chat) as conv:
@@ -15,8 +13,9 @@ async def _(event):
             await conv.send_message("/start")
             await asyinco.sleep(1)
             await conv.send_message(/setname)
-            await asyinco.sleep(0.1)
+            await asyinco.sleep(1)
             await conv.send_message(TBOT)
-            await asyinco.sleep(0.1)
+            await asyinco.sleep(1)
             await conv.send_message(ALIVE_NAME)
+            await event.client.send_read_acknowledge(conv.chat_id)
             await e.edit("uuuuu")
