d0d6713b4acd0207d19689ad135aac06462fdd69
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-18 05:12:32 +0300

    .2k

--- userbot/utils.py
+++ userbot/utils.py
@@ -44,7 +44,7 @@ def load_module(shortname):
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
-        print("{} " + shortname).format(usr)
+        print(f"{usr} " + shortname)
     else:
         import userbot.utils
 
@@ -95,7 +95,7 @@ def load_module(shortname):
         spec.loader.exec_module(mod)
         # for imports
         sys.modules["userbot.plugins." + shortname] = mod
-        print("{}" + shortname).format(usr)
+        print(f"{usr}" + shortname)
 
 def remove_plugin(shortname):
     try:
