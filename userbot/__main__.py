2a94ddd8382c3845925f8f4d6e8a7d76550ed5dc
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-18 19:25:56 +0300

    !?

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -44,7 +44,7 @@ else:
     try:
         if TBOT is not None:
             print("⫷ يتم تحميل انلاين اكسس ⫸")
-            bot.tgbot = TelegramClient(Xtbot, api_id=Xapid, api_hash=Xhash).start(bot_token=Xtoken)
+            bot.tgbot = TelegramClient(Xtbot, api_id=Xapid, api_hash=Xhash).start(bot_token=Xtoke)
             print("⫷ اكتمل تنزيل انلاين اكسس بدون اخطاء ⫸")
             print("⫷ يتم بدء بوت اكسس ⫸")
             bot.loop.run_until_complete(add_bot(TBOT))
