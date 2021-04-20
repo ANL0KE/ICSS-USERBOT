fbd0eec394e97b314b091792cf4f59c3d9e63444
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-20 05:13:42 +0300

    - Ok

--- userbot/plugins/ttest.py
+++ userbot/plugins/ttest.py
@@ -1,13 +1,13 @@
-import datetime
-
+from datetame import datetime
 
 @icssbot.on(
-    icss_cmd(pattern="العمر$")
+    icss_cmd(pattern="ها")
 )
 async def _(e):
     icst = e.txt
-    yar = icst[6:11]
-    Dbt = yar
+    yar = icst[4:9]
+    if not yar:
+       await eor(e, "هها هلو")
     YearNow = datetime.now().year
-    MyAge = (int(YearNow) - int(Dbt))
+    MyAge = (int(YearNow) - int(yar))
     await eor(e, "عمرك هوه {}".format(MyAge))
