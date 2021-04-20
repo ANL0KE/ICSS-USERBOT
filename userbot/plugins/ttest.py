4b4d84bda31f2d8078ddee70b4c57571e02f6960
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-20 04:56:02 +0300

    - Hm

---
+++ userbot/plugins/ttest.py
@@ -0,0 +1,14 @@
+import datetime
+
+emsg = "يجب ادخال مواليدك السنه فقط"
+
+@icssbot.on(
+    icss_cmd(pattern="العمر$")
+)
+async def _(e):
+    icst = e.txt
+    yar = icst[6:11]
+    Dbt = yar
+    YearNow = datetime.now().year
+    MyAge = (int(YearNow) - int(Dbt))
+    await eor(e, "عمرك هوه %s"% MyAge)
