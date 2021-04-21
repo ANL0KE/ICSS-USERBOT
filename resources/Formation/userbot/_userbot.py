46f44127eedeb4b9dcaeec5e378f95a7004bd7fc
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-21 03:33:53 +0300

    - Ok

---
+++ resources/Formation/userbot/_userbot.py
@@ -0,0 +1,86 @@
+# Hey There
+
+import os
+import sys
+import time
+from distutils.util import strtobool as sb
+from logging import DEBUG, INFO, basicConfig, getLogger
+
+import heroku3
+from dotenv import load_dotenv
+from requests import get
+from telethon import TelegramClient 
+from telethon.sessions import StringSession 
+
+from userbot.Config import Config
+from userbot.tosh import Tlk
+
+StartTime = time.time()
+icsv = "1.0.0"
+
+ICS_ID = ["1588663614", "1233775025"]
+
+# for print :
+usr = "ICSS USERBOT - {}".format(Tlk)
+adn = "ICSS ADMIN TOOLS - {}".format(Tlk)
+ani = "ICSS ANIMATIONS - {}".format(Tlk)
+tsh = "ICSS TOSHA - {}".format(Tlk)
+ast = "ICSS ASSISTANT {}".format(Tlk)
+
+CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+
+if CONSOLE_LOGGER_VERBOSE:
+    basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=DEBUG,
+    )
+else:
+    basicConfig(
+        format="[%(asctime)s]- %(name)s- %(levelname)s- %(message)s",
+        level=INFO,
+        datefmt="%m-%d %H:%M:%S",
+    )
+LOGS = getLogger(__name__)
+
+
+try:
+    if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
+        HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[
+            Config.HEROKU_APP_NAME
+        ]
+    else:
+        HEROKU_APP = None
+except Exception:
+    HEROKU_APP = None
+
+# Global Configiables
+COUNT_MSG = 0
+USERS = {}
+COUNT_PM = {}
+LASTMSG = {}
+CMD_HELP = {}
+ISAFK = False
+AFKREASON = None
+CMD_LIST = {}
+SUDO_LIST = {}
+# for later purposes
+INT_PLUG = ""
+LOAD_PLUG = {}
+
+if Config.STRING_SESSION:
+    session_name = str(Config.STRING_SESSION)
+    try:
+        if session_name.endswith("="):
+            bot = TelegramClient(
+                StringSession(session_name), Config.APP_ID, Config.API_HASH
+            )
+        else:
+            bot = TelegramClient(
+                "TG_BOT_TOKEN", api_id=Config.APP_ID, api_hash=Config.API_HASH
+            ).start(bot_token=Config.STRING_SESSION)
+    except Exception as e:
+        LOGS.warn(f"STRING_SESSION - {str(e)}")
+        sys.exit()
+else:
+    session_name = "startup"
+    bot = TelegramClient(session_name, Config.APP_ID, Config.API_HASH)
