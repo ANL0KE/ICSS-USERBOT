464c17a7665731d93011fa9afb1b95d0696f05a5
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-21 06:42:27 +0300

    - Ok

--- resources/Config/_config.py
+++ resources/Config/_config.py
@@ -5,7 +5,7 @@ import os
 ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
-    from .icss_config import Config  # @rruuurr
+    from ._icsconfig import Config  # @rruuurr
 else:
     if os.path.exists("config.py"):
         from config import Development as Config  # Hey there
