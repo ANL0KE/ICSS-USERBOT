b0cf54732fbf458b1a528782aa797226dbd7a68d
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-23 06:31:31 +0300

    - Ok

--- userbot/plugins/calc.py
+++ userbot/plugins/calc.py
@@ -6,27 +6,33 @@ SOURCE ICSS
 import io
 import sys
 import traceback
+from ..tosh import C
 
 
-@icssbot.on(admin_cmd(pattern="calc (.*)"))
-@icssbot.on(sudo_cmd(pattern="calc (.*)", allow_sudo=True))
-async def _(car):
-    cmd = car.text.split(" ", maxsplit=1)[1]
-    event = await edit_or_reply(car, "Calculating ...")
-    old_stderr = sys.stderr
-    old_stdout = sys.stdout
-    redirected_output = sys.stdout = io.StringIO()
-    redirected_error = sys.stderr = io.StringIO()
+@icssbot.on(
+    icss_cmd(pattern="حاسبه (.*)")
+)
+@icssbot.on(sudo_cmd(
+               pattern="حاسبه (.*)", 
+               allow_sudo=True)
+           )
+async def _(ics):
+    tosh = ics.text.split(" ", maxsplit=1)[1]
+    tsh = await eor(ics, "**⌔∮ يتم الحل… **")
+    osr = sys.stderr
+    ost = sys.stdout
+    rot = sys.stdout = io.StringIO()
+    reor = sys.stderr = io.StringIO()
     stdout, stderr, exc = None, None, None
-    san = f"print({cmd})"
+    kim = "print(%s)"% tosh
     try:
-        await aexec(san, event)
+        await aexec(kim, tsh)
     except Exception:
         exc = traceback.format_exc()
-    stdout = redirected_output.getvalue()
-    stderr = redirected_error.getvalue()
-    sys.stdout = old_stdout
-    sys.stderr = old_stderr
+    stdout = rot.getvalue()
+    stderr = reor.getvalue()
+    sys.stdout = ost
+    sys.stderr = osr
     evaluation = ""
     if exc:
         evaluation = exc
@@ -36,10 +42,8 @@ async def _(car):
         evaluation = stdout
     else:
         evaluation = "Sorry I can't find result for the given equation"
-    final_output = "**EQUATION**: `{}` \n\n **SOLUTION**: \n`{}` \n".format(
-        cmd, evaluation
-    )
-    await event.edit(final_output)
+    fot = (C.format(tosh, evaluation)
+    await eor(tsh, fot)
 
 
 async def aexec(code, event):
@@ -47,10 +51,10 @@ async def aexec(code, event):
     return await locals()["__aexec"](event)
 
 
-CMD_HELP.update(
+tosh_HELP.update(
     {
         "calc": "**Plugin : **`calc`\
-        \n\n**Syntax : **`.calc expression` \
-        \n**Function : **solves the given maths equation by BODMAS rule. "
+        \n\n• **Syntax : **`.حاسبه ` \
+        \n• **Function : **للحساب مثال (2*2) "
     }
 )
