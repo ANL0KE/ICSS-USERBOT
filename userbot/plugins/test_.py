ca176826e673fabcdeb8d42808dfca47adffde75
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-26 00:07:04 +0300

    - Ok

--- userbot/plugins/test_.py
+++
@@ -1,9 +0,0 @@
-Pic = "https://telegra.ph/file/91fff8d40afdf0bcf626b.jpg"
-F = "- نـوࢪت "
-
-@icss.on(icss_cmd(patter="ila"))
-async def tt(icss):
-    ics_id = await rd(icss)
-    if Pic:
-        await icss.client.send_file(icss.chat_id, F, Pic, reply_to=ics_id)
-        await icss.delete()
