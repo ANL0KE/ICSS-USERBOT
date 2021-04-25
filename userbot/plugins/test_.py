529cd93a651a10750bd3a648ca1038afe3670db7
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-26 00:04:15 +0300

    - Ok

--- userbot/plugins/test_.py
+++ userbot/plugins/test_.py
@@ -3,6 +3,7 @@ F = "- نـوࢪت "
 
 @icss.on(icss_cmd(patter="ila"))
 async def tt(icss):
+    ics_id = await rd(icss)
     if Pic:
-        await icss.client.send_file(icss.chat_id, F)
+        await icss.client.send_file(icss.chat_id, F, Pic, reply_to=ics_id)
         await icss.delete()
