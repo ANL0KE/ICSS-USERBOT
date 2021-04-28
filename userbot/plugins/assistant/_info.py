5afa436a5eb410bdc3d0a711e2c914c1365b8c49
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-29 02:00:32 +0300

    - Ok

--- userbot/plugins/assistant/_info.py
+++ userbot/plugins/assistant/_info.py
@@ -2,6 +2,7 @@
 # translater for Icss - Userbot
 # Owner ~ <@rruuurr>
 
+from . import Asstid
 from telethon.utils import pack_bot_file_id
 
 @asst_cmd("الايدي")
@@ -13,19 +14,16 @@ async def _(e):
         if r_msg.media:
             bot_api_file_id = pack_bot_file_id(r_msg.media)
             await asst.send_message(
-                e.chat_id,
-                "Current Chat ID: `{}`\nFrom User ID: `{}`\nBot API File ID: `{}`".format(
-                    str(e.chat_id), str(r_msg.sender_id), bot_api_file_id
-                ),
+                e.chat_id, Asstid[0].form(str(e.chat_id),
+                                          str(r_msg.sender_id), 
+                                          bot_api_file_id),
             )
         else:
             await asst.send_message(
-                e.chat_id,
-                "Current Chat ID: `{}`\nFrom User ID: `{}`".format(
-                    str(e.chat_id), str(r_msg.sender_id)
-                ),
+                e.chat_id, Asstid[1].format(str(e.chat_id),
+                                            str(r_msg.sender_id)),
             )
     else:
         await asst.send_message(
-            e.chat_id, "Current Chat ID: `{}`".format(str(e.chat_id))
+            e.chat_id, Asstid[2].format(str(e.chat_id))
         )
