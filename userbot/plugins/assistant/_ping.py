10aeb5e520d0d39d04a98f1f20e3baf9fe13ef0b
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-28 10:18:16 +0300

    - Ok

---
+++ userbot/plugins/assistant/_ping.py
@@ -0,0 +1,16 @@
+import time
+from telethon import events
+from datetime import datetime
+from . import *
+from .. import StartTime
+
+
+@tgbot.on(
+    events.NewMessage(pattern=("بنك"))
+)
+async def _(e):
+    Start = datetime.now()
+    End = datetime.now()
+    Ms = (end - start).microseconds / 1000
+    UpTime = get_readable_time((time.time() - StartTime))
+    await tgbot.send_message(e.chat_id, Ping.format(Ms, UpTime)
