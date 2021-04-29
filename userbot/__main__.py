7e02947c07c4a20f60714b27bda4302a7eb9381f
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-29 11:08:36 +0300

    - Ok

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -10,6 +10,7 @@ import telethon.utils
 from telethon import TelegramClient as TC
 
 from userbot.tosh import *
+from resources import *
 from userbot import LOGS, bot
 from userbot.Config import Config
 from userbot.utils import (
@@ -144,6 +145,7 @@ for name in Kim:
 print(ICSW)
 print(Message.format(ALIVE_NAME, TBOT))
 
+bot.loop.create_task(setbot())
 bot.loop.create_task(startupmessage())
 
 if len(sys.argv) not in (1, 3, 4):
