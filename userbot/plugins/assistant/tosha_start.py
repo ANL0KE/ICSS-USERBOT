dbaeb34601916d0e46b4a99d9eea72af7083a381
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-29 10:52:43 +0300

    - Ok

--- userbot/plugins/assistant/tosha_start.py
+++ userbot/plugins/assistant/tosha_start.py
@@ -34,7 +34,7 @@ auth_url = r["auth_url"]
 # start-others
 @tgbot.on(events.NewMessage(pattern="^/start"))  # pylint: disable=oof
 async def start_all(event):
-    if event.chat_id != bot.uid:
+    if event.chat_id != OWNER_ID:
         return
     target = event.sender_id
     if present_in_userbase(target):
