4f65cb3231c4142c2c558abce055e2c177bb5fde
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-04-29 13:19:48 +0300

    - Ok

---
+++ userbot/plugins/assistant/PmTosh/incoming.py
@@ -0,0 +1,21 @@
+from userbot.plugins.assistant.sql.users_sql import add_user_to_db
+from userbot.plugins.assistant.sql.blacklist_sql import check_is_black_list
+from telethon import events
+from userbot.plugins import TOSHA_ID
+
+# if incoming
+@asst.on(events.NewMessage(func=lambda e: e.is_private))
+async def _(e):
+    incoming = e.raw_text
+    who = e.sender_id
+    if check_is_black_list(who):
+        return
+    if incoming.startswith("/"):
+        pass
+    elif who == TOSHA_ID:
+        return
+    else:
+        await e.get_sender()
+        e.chat_id
+        to = await e.forward_to(TOSHA_ID)
+        add_user_to_db(to.id, who, e.id)
