5494eb831618c72fcf4e6183e58305a5d0772575
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-05-01 19:16:18 +0300

    - Ok

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,4 +1,5 @@
-from userbot.tosh import Calc, C # calc text
+# for Calc text & HelpText - HelpString
+from userbot.tosh import Calc, C, HelpString
 # for plugins __init__
 from resources.Formation.plugins import *
 
