5fa07dfa60c52d3b257d4de2ac5d62ddc00253b4
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-05-16 12:19:11 +0300

    -

--- userbot/plugins/app.py
+++ userbot/plugins/app.py
@@ -6,16 +6,14 @@
 import bs4
 import requests
 
-from . import ALIVE_NAME
-
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
+from . import mention
 
 
-@icssbot.on(admin_cmd(pattern="app (.*)"))
-@icssbot.on(sudo_cmd(pattern="app (.*)", allow_sudo=True))
+@icss.on(icss_cmd(pattern="برنامج (.*)"))
+@icss.on(sudo_cmd(pattern="برنامج (.*)", allow_sudo=True))
 async def apk(event):
     app_name = event.pattern_match.group(1)
-    event = await edit_or_reply(event, "Searching!")
+    event = await eor(event, "**⌔∮ ججاري البحث عـن التطبيق**")
     try:
         remove_space = app_name.split(" ")
         final_name = "+".join(remove_space)
@@ -52,113 +50,39 @@ async def apk(event):
             .findNext("div", "uzcko")
             .img["data-src"]
         )
-        app_details = "<a href='" + app_icon + "'>📲&#8203;</a>"
-        app_details += " <b>" + app_name + "</b>"
+        app_details = "<a href='" + app_icon + "'>𓆰&#8203;</a>"
+        app_details += " <b>" + app_name + " 𓆪</b>"
         app_details += (
-            "\n\n<code>Developer :</code> <a href='"
+            "\n\n<code>**⌔∮ المطور ~**</code> <a href='"
             + app_dev_link
             + "'>"
             + app_dev
             + "</a>"
         )
-        app_details += "\n<code>Rating :</code> " + app_rating.replace(
-            "Rated ", "⭐ "
+        app_details += "\n<code>**⌔∮ تقييم التطبيق ~</code> " + app_rating.replace(
+            "Rated ", "☆ "
         ).replace(" out of ", "/").replace(" stars", "", 1).replace(
-            " stars", "⭐ "
+            " stars", "☆ "
         ).replace(
             "five", "5"
         )
         app_details += (
-            "\n<code>Features :</code> <a href='"
+            "\n<code>**⌔∮ للتحميل ~**</code> <a href='"
             + app_link
-            + "'>View in Play Store</a>"
+            + "'>لتحميلها من سوق بلي</a>"
         )
-        app_details += f"\n\n===> {DEFAULTUSER} <==="
+        app_details += f"\n\n===> {mention} <==="
         await event.edit(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
         await event.edit("No result found in search. Please enter **Valid app name**")
     except Exception as err:
         await event.edit("Exception Occured:- " + str(err))
-
-
-@icssbot.on(admin_cmd(pattern="appr (.*)"))
-@icssbot.on(sudo_cmd(pattern="appr (.*)", allow_sudo=True))
-async def apkr(event):
-    app_name = event.pattern_match.group(1)
-    event = await edit_or_reply(event, "searching!")
-    try:
-        remove_space = app_name.split(" ")
-        final_name = "+".join(remove_space)
-        page = requests.get(
-            "https://play.google.com/store/search?q=" + final_name + "&c=apps"
-        )
-        str(page.status_code)
-        soup = bs4.BeautifulSoup(page.content, "lxml", from_encoding="utf-8")
-        results = soup.findAll("div", "ZmHEEd")
-        app_name = (
-            results[0].findNext("div", "Vpfmgd").findNext("div", "WsMG1c nnK0zc").text
-        )
-        app_dev = results[0].findNext("div", "Vpfmgd").findNext("div", "KoLSrc").text
-        app_dev_link = (
-            "https://play.google.com"
-            + results[0].findNext("div", "Vpfmgd").findNext("a", "mnKHRc")["href"]
-        )
-        app_rating = (
-            results[0]
-            .findNext("div", "Vpfmgd")
-            .findNext("div", "pf5lIe")
-            .find("div")["aria-label"]
-        )
-        app_link = (
-            "https://play.google.com"
-            + results[0]
-            .findNext("div", "Vpfmgd")
-            .findNext("div", "vU6FJ p63iDd")
-            .a["href"]
-        )
-        app_icon = (
-            results[0]
-            .findNext("div", "Vpfmgd")
-            .findNext("div", "uzcko")
-            .img["data-src"]
-        )
-        app_details = "<a href='" + app_icon + "'>📲&#8203;</a>"
-        app_details += " <b>" + app_name + "</b>"
-        app_details += (
-            "\n\n<code>Developer :</code> <a href='"
-            + app_dev_link
-            + "'>"
-            + app_dev
-            + "</a>"
-        )
-        app_details += "\n<code>Rating :</code> " + app_rating.replace(
-            "Rated ", "⭐ "
-        ).replace(" out of ", "/").replace(" stars", "", 1).replace(
-            " stars", "⭐ "
-        ).replace(
-            "five", "5"
-        )
-        app_details += (
-            "\n<code>Features :</code> <a href='"
-            + app_link
-            + "'>View in Play Store</a>"
-        )
-        app_details += "\n\n<b>Download : </b> <a href='https://t.me/joinchat/JCu-H1NikiYDgNjpjPYd4A'>Request_Here</a>"
-        app_details += "\n\n===> @Xpl0iter <==="
-        await event.edit(app_details, link_preview=True, parse_mode="HTML")
-    except IndexError:
-        await event.edit("No result found in search. Please enter **Valid app name**")
-    except Exception as err:
-        await event.edit("Exception Occured:- " + str(err))
-
 
 CMD_HELP.update(
     {
         "app": "**Plugin :** `app`\
         \n**Syntax : **`.app [app name]`\
         \n**Usage: **searches the app in the playstore and provides the link to the app in playstore and fetchs app details \
-        \n\n**Syntax : **`.appr [app name]`\
-        \n**Usage: **searches the app in the playstore and provides the link to the app in playstore and fetchs app details with Xpl0iter request link. \
         "
     }
 )
