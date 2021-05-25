9102f21a68c86d52f71ae12247ce7044fd3ace6a
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-05-25 15:21:08 +0300

    -

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,11 +1,20 @@
 # for plugins:
 from resources.Formation.plugins import *
+from userbot import bot
+from userbot.tosh import *
+
+# For format M:
+O = bot.me.id
+Name = bot.me.first_name
+M = "[{}](tg://user?id={})".format(Name, O)
+
 
 # for Calc text - HelpText & Quotly text
 from userbot.tosh import (
     Calc,
     C,
     HelpString,
-    Quotly
+    Quotly,
+    Echo
 )
 
