e83f900a76ca8da6afd3d82f2de59026849d4839
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-05-26 14:28:30 +0300

    -

--- userbot/plugins/pms.py
+++ userbot/plugins/pms.py
@@ -16,7 +16,7 @@ from . import (
 )
 from .sql_helper import pmpermit_sql as pmpermit_sql
 
-MAX_FLOOD_IN_PMS = Config.MAX_FLOOD_IN_P_M_S 
+MAX_FLOOD_IN_PMS = Config.MAX_FLOOD_IN_P_M_s
 PM_WARNS = {}
 PREV_REPLY_MESSAGE = {}
 CACHE = {}
